4665db726e01b8e775750adab629c0bcea235025
Kamal <kamal.raghe33@gmail.com>
2024-03-11 18:55:44 -0400

    please work

---
+++ pages/home.js
@@ -0,0 +1,225 @@
+
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { addDoc, collection, onSnapshot, query, where, getDoc } from "firebase/firestore"
+import Link from "next/link"
+import { initFirebase } from "@/firebase"
+import { getFirestore } from "firebase/firestore"
+import { db , auth} from "../firebase"
+import { getFunctions, httpsCallable } from "firebase/functions";
+
+export default function Home() {
+  const [sign, setSign] = useState(false)
+  const [add, setAdd]= useState(false)
+  const app = initFirebase()
+  const [cancel, setCancel] = useState(false)
+  const [color, setColor] = useState('blue')
+  const [link, setLink] = useState(false)
+  const [date, setDate] = useState(Date.now()) 
+  const [free, setFree] = useState(false)
+  const router = useRouter()
+  const [payed, Payed] = useState(false)
+  const [Account, SetAccounts] = useState([])
+    const [account, setAccount] = useState({
+    title:""
+    })
+
+    function SignOut(){
+      auth.signOut().then(() => {router.push('/')}).catch((error) => alert(error.message))
+      window.localStorage.setItem('User', '')
+      window.localStorage.setItem('uid', '')
+    }
+
+    // async function getCheckoutUrl(){
+    //   const app = initFirebase()
+    //   const userId = window.localStorage.getItem('uid');
+    
+    //   const db = getFirestore(app);
+    //   const checkoutSessionRef = collection(
+    //     db,
+    //     "customers",
+    //     userId,
+    //     "checkout_sessions"
+    //   );
+    
+    //   const docRef = await addDoc(checkoutSessionRef, {
+    //     price: 'price_1OqoPfIZAnJ0s9ybKIwjY1VR',
+    //     success_url: window.location.origin,
+    //     cancel_url: window.location.origin,
+    //   });
+    //   const promise = new Promise((resolve, reject) => {
+    //     const unsubscribe = onSnapshot(docRef, (snap) => {
+    //       const { error, url } = snap.data() || {};
+    //       if (error) {
+    //         unsubscribe(); 
+    //         reject(new Error(`An error occurred: ${error.message}`));
+    //       }
+    //       if (url) {
+    //         unsubscribe(); 
+    //         router.push(url);
+    //       }
+    //     });
+    //   });
+    // };
+
+    // async function PayedCheck() {
+    //   const app = initFirebase()
+    //     const userId = window.localStorage.getItem('uid')
+    //   const db = getFirestore(app);
+    //   const subscriptionsRef = collection(db, "customers", userId, "subscriptions");
+    //   const q = query(
+    //     subscriptionsRef,
+    //     where("status", "in", ["trialing", "active"])
+    //   );
+    
+    //   const promise = new Promise((resolve, reject) => {
+    //     const unsubscribe = onSnapshot(
+    //       q,
+    //       (snapshot) => {
+    
+    //         if (snapshot.docs.length === 0) {
+    //           Payed(false);
+    //         } else {
+    //           Payed(true);
+    //         }
+    //         unsubscribe();
+    //       },
+    //       reject
+    //     );
+    //   });
+    // };
+
+    async function getPortalUrl(){
+  
+    }
+
+    async function handleSubmit(){
+      const user = window.localStorage.getItem('User')  
+      if(account.title){
+      const docRef = await addDoc(collection(db, user), {
+            title: account.title,
+            count: link
+        })
+        setAccount({title:""})
+        setAdd(true)
+      }
+        
+    }
+
+    async function Free(){
+      const num = Math.ceil(Math.random()*100000000000000000000)
+      window.localStorage.setItem('id' , num)
+      setFree(num)
+      const user = window.localStorage.getItem('User')  
+      const docRef = await addDoc(collection(db, user), {
+            title: 'Free trial',
+            count: 7*24*60*60*1000+Date.now()  
+        })
+        
+      
+        
+    }
+
+    function update(){
+      setDate(requestAnimationFrame(update))
+    }
+
+    useEffect(()=>{
+      // PayedCheck()
+      const security = window.localStorage.getItem('Id')
+      if(security){
+        setLink(security)
+        console.log(security)
+      }else{
+        const num = Math.ceil(Math.random()*100000000000000000000)
+        window.localStorage.setItem('Id' , num)
+        setLink(num)
+      }
+      setFree(window.localStorage.getItem('id'))
+      const user = window.localStorage.getItem('User')
+      const unsubscribe = onSnapshot(collection(db, user), (snap) =>{
+           
+        SetAccounts(snap.docs.map(doc => {
+            return{
+                id: doc.id,
+                title: doc.data().title, 
+                count: doc.data().count 
+            }
+        }))
+    })
+    return unsubscribe
+    },[])
+
+
+    useEffect(()=>{
+      console.log(payed)
+    },[payed])
+
+    useEffect(()=>{
+      if(add === true){
+        router.push('/home')
+      }
+      setTimeout(()=>{
+        setAdd(false)
+      },1000)
+    },[add])
+
+    function security(id){
+      window.localStorage.setItem('ID' , id)
+    }
+
+    useEffect(()=>{
+      console.log(cancel)
+      if(cancel === true){
+      setColor('red')
+    }else{
+      setColor('blue')
+    }
+    },[cancel])
+ 
+  return (
+  <div className="center beige column">
+      <div className="center column">
+        {add && <div>Adding Account...</div>}
+          {<h1 className="center column">{Account.map(acc => {
+            return <div className="center sb" style={{width:"340px"}} key = {acc.id}>
+              {acc.count - Date.now() > 0 && acc.title === 'Free trial' && <Link className="center sb" style={{textDecoration:'none'}} href={`/${acc.id}/${acc.title}/math`}>
+                <span >
+                  <button onClick={()=>{security(acc.id)}} 
+                  style={{fontSize:"30px",border:"none",backgroundColor:"beige",color:'blue',textDecoration:'underline'}}>
+                    {acc.title} 
+                  </button>
+                </span>
+              </Link>}
+              { acc.count - Date.now() > 0 && acc.title === 'Free trial' && <div className="font">{Math.floor(((acc.count - Date.now())%(1000*60*60*24*7))/1000/60/60/24)}d {""}{Math.floor(((acc.count - Date.now())%(1000*60*60*24))/1000/60/60)}h {""}{Math.floor(((acc.count - Date.now())%(1000*60*60))/1000/60)}m {""}
+                {Math.floor(((acc.count - Date.now())%(1000*60))/1000)}s</div>}
+            </div>
+        })}</h1>}
+      
+        {!add && payed && <h1 className="center column">{Account.map(acc => {
+            return <div className="center sb" key = {acc.id}>
+              { acc.count === link && <Link className="center sb" style={{width:"250px",textDecoration:'none'}} href={`/${acc.id}/${acc.title}/math`}>
+                <span>
+                  <button onClick={()=>{security(acc.id)}} 
+                  style={{fontSize:"30px",border:"none",backgroundColor:"beige",color: color,textDecoration:'underline'}}>
+                    {acc.title} 
+                  </button>
+                </span>
+              </Link>}
+              { acc.count === link && <button className="relative center" style={{border:"none",fontSize:"30px",backgroundColor:"lightblue",padding:'2px 10px',borderRadius:"15px",color:"black",textDecoration:'none'}} >edit</button>}
+            </div>
+        })}</h1>}
+
+        {payed && (Account.length === 1 || Account.length === 0) && <div className="center">
+            <input placeholder="Create account" value={account.title} type='text' onChange = {(e) => setAccount({...account, title: e.target.value})}></input>
+            <button onClick={() => handleSubmit()}>Enter</button>
+           
+        </div>
+        }{Account.length === 0 && !free && <button style={{backgroundColor:"yellow",color:'black',width:"100px",height:'25px',borderRadius:"20px",margin:'10px'}} onClick={Free} >Free trial</button>}
+        {!(Account.length === 0 && !free) && !payed && <button style={{backgroundColor:"orange",color: 'white',width:"100px",height:'40px',margin:'15px',borderRadius:"20px"}} onClick={getCheckoutUrl} >CA$3.99</button>} 
+        {payed && <button className="topic" style={{backgroundColor:"red",width:"100px"}} onClick={getPortalUrl} >Cancel</button>}
+        {!add && <button onClick={SignOut} >SignOut</button>}
+      </div>
+  </div>
+  )
+}
---
+++ pages/stats.js
@@ -0,0 +1,136 @@
+
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import { auth } from "@/firebase"
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth"
+
+export default function Home() {
+  const [sign, setSign] = useState()
+  const [create, setCreate] = useState(false)
+  const [user, setUser] = useState()
+  const [red, setRed] = useState()
+  const [ loaded, setLoaded] = useState(false)
+  const [password, setPassword] = useState('password')
+  const [move, setMove] = useState('165px')
+  const [id, setId] = useState('id')
+
+  const router = useRouter()
+
+
+
+  function signIn(e){
+    e.preventDefault()
+    const email = e.target.email.value
+    const password = e.target.password.value
+    signInWithEmailAndPassword(auth,email,password).then((result)=>{
+      setUser(true)
+    }).catch(err =>{
+      setRed('red')
+      alert(err)
+      console.log(err)
+      setTimeout(() => {
+        setRed(false)
+      }, 1000);
+    })
+  }
+  
+  function CreateUser(e){
+    e.preventDefault()
+
+    const email = e.target.email.value
+    const password = e.target.password.value
+    
+    createUserWithEmailAndPassword(auth,email,password).then(()=>{
+      setUser(true)
+    }).catch(err =>{
+      alert(err)
+      setRed('red')
+      setTimeout(() => {
+        setRed(false)
+      }, 1000);
+    })
+  }
+
+  function Password(){
+    if(password === 'password'){
+      setPassword('')
+      setMove('169px')
+    }else{
+      setPassword('password')
+      setMove('165px')
+    }
+  }
+
+  useEffect(()=>{
+    const SignInUser = window.localStorage.getItem('User')
+    setUser((SignInUser))
+    setLoaded(true)
+  },[])
+
+  useEffect(()=>{
+    if(user === true){
+    window.localStorage.setItem('User', `${auth.currentUser?.email}`)
+    window.localStorage.setItem('uid', `${auth.currentUser?.uid}`)
+}},[user])
+
+  return (
+    <div className="center beige column">
+      <div className="center column" style={{alignItems:"start"}}>
+        <div className="relative" style={{fontSize:"70px",top:"20px",color:'navy',padding:"0px"}} >Math</div>
+        <div className="relative" style={{fontSize:"70px",color:'purple',paddingBottom:"20px"}} >Improved</div>
+        {!user && !create && loaded && <button className="relative center" onClick={Password} style={{padding:"0px",top:"125px",left:move,backgroundColor:"white",border:"none"}} >
+            <span className="hide" ></span><div className="relative">{password === 'password' ? 'show' : 'hide'}</div></button>}
+        {!user && !create && loaded && <div>
+          <div style={{paddingBottom:'10px',fontSize:"50px"}} >Sign In <button style={{background:'none',fontSize:"30px",border:'none',color:"blue"}} onClick={()=>{setCreate(true)}} >Sign up</button></div>  
+        <form className="center sb column" style={{alignItems:'start'}} onSubmit={(e)=> signIn(e)}>
+          <input className="relative" style={{borderColor: red,width:'190px'}} name='email' type='email' placeholder="Email" ></input><br></br>
+          <div className="center" style={{width:"200px",backgroundColor:"transparent",border:'none'}} >
+            <input style={{borderColor: red,width:'200px'}} name='password' type={password} placeholder="password" ></input>
+        </div>
+        <div className="center" >
+          <button style={{marginTop:'10px'}}>Enter</button>   
+        </div>
+        </form>
+        </div>}
+      </div>
+      {(create || user) && <div className="box" ></div>}
+      {create && <div className="box" ></div>}
+      
+        {!user && create && <div className="timeout center" style={{border:'10px solid navy',backgroundColor:"silver"}} >
+        <button className="relative" style={{top:'-65px',left:'250px',background:'none',border:'none',color:"black",fontSize:"50px"}} onClick={()=>{setCreate(false)}} >X</button> 
+        <div>
+          <button className="relative center" onClick={Password} style={{width:"40px",zIndex:"10",left:"130px",top:'125px',backgroundColor:"transparent",border:"none"}} >
+              <span className="hide" ></span><div>{password === 'password' ? 'show' : 'hide'}</div></button>
+          <form className="center column relative" style={{right:"20px"}} onSubmit={(e)=> CreateUser(e)}>
+            <div style={{padding:'10px',fontSize:"40px"}} >Sign Up</div>
+            <input style={{borderColor: red, width:'180px'}} name='email' type='email' placeholder="Email" ></input><br></br>
+            <div className="center" style={{backgroundColor:"transparent",border:'none'}} >
+              <input style={{borderColor: red, width:'180px'}} name='password' type={password} placeholder="password" ></input>
+          </div><br></br>
+              <button style={{marginBottom:'10px'}} >Create</button>
+          </form>
+        
+        </div>
+        </div>}
+      <div className="box" ></div>
+      {!user && <div className="box" ></div>}
+      {user && <div className="box center" style={{width:'340px'}} >
+        
+        <button className="topic column" onClick={()=>{router.push('/stats')}} style={{width:'150px', height:"90px"}} >
+          <div className="center" style={{paddingTop:'20px',paddingBottom:"5px"}} >
+            <div className="red relative" style={{marginBottom:"10px",rotate:'90deg',width:'20px',height:'10px',left:'20px',top:"4px"}} ></div>
+            <div className="relative" style={{marginBottom:"10px",rotate:'90deg',width:'40px',height:'10px',backgroundColor:"cyan",top:"-5px",left:"5px"}} ></div>
+            <div className="relative" style={{marginBottom:"10px",rotate:'90deg',width:'30px',height:'10px',left:"-15px",backgroundColor:"lime"}} ></div>
+          </div>
+          Stats
+        </button>
+        <button className="topic column" onClick={()=>{router.push('/home')}} style={{width: '150px', height:"90px"}} >
+          <div className="center" style={{padding:'0 20px'}}>
+            <div className="relative" style={{rotate:"90deg",borderBottom:"50px solid white",borderRight:'25px solid transparent',borderLeft:'25px solid transparent'}} ></div>
+          </div>
+          Start
+        </button>
+      </div>}
+    </div>
+  )
+}
