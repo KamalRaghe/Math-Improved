516fd4be1b8cc60cf2adb289d2588b197980b4c1
Kamal <kamal.raghe33@gmail.com>
2024-08-27 20:54:48 -0400

    back to norm

--- pages/index.js
+++ pages/index.js
@@ -110,15 +110,15 @@ export default function Home() {
           </div>  
         <form className="center sb column" style={{alignItems:'start'}} onSubmit={(e)=> signIn(e)}>
           
-          <div className="" style={{width:"150px",backgroundColor:"transparent",border:'none'}} >
-            <input className="relative" style={{borderColor: red,width:'150px',marginBottom:"10px"}} name='email' type='email' placeholder="Email" ></input>
-            <input style={{borderColor: red,width:'150px'}} name='password' type={password} placeholder="password" ></input> </div>
+          <div className="" style={{width:"200px",backgroundColor:"transparent",border:'none'}} >
+            <input className="relative" style={{borderColor: red,width:'190px',marginBottom:"10px"}} name='email' type='email' placeholder="Email" ></input>
+            <input style={{borderColor: red,width:'190px'}} name='password' type={password} placeholder="password" ></input> </div>
         <div className="center" >
           <button style={{position:'relative',top:"30px"}}>Enter</button>   
         </div>
         </form>
         </div>}
-        <div className="relative" style={{display:'flex',justifyContent:'end',width:'200px',top:'-74px',left:"13px"}}>
+        <div className="relative" style={{display:'flex',justifyContent:'end',width:'200px',top:'-74px',left:"35px"}}>
             {!user && !create && loaded && <button onClick={Password}>
             <div className="relative">{password === 'password' ? 'show password' : 'hide password'}</div></button>}
         </div>
