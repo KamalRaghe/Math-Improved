f9d11aa74effb73685265de4adb6edaa1ed5a5f6
Kamal <kamal.raghe33@gmail.com>
2024-09-17 17:18:53 -0400

    commit

--- pages/Enter.js
+++ pages/Enter.js
@@ -12,18 +12,24 @@ function App() {
  const [account, setAccount] = useState({
     title:""
     })
+    const [code, setCode] = useState({
+        title:""
+        })
 
-    function updateList(){
-        set(ref(rdb, 'users/' + '3222/' + '-O68N6PLEqfBXLJrFPLv'),{
-            title:account.title,
-        }).then(()=>{
-            console.log('done Added to list ')
+    function inList(){
+        const usersRef = ref(rdb, `${code.title}`)
+        get(usersRef).then((snapshot)=>{
+            if(snapshot.exists()){
+                window.localStorage.setItem('GameRoom', code.title)
+                setAdd(true)
+                console.log(code.title)
+            }
         })
     }
 
     function AddList(){
         setAdd(true)
-        const usersRef = ref(rdb, 'users/')
+        const usersRef = ref(rdb, `${code.title}`)
         const AddList = push(usersRef)
         set(AddList,{
             user: account.title
@@ -41,8 +47,7 @@ function App() {
                         window.localStorage.setItem('GameName',usersArray[i].data.user)
                     }
                 }
-                let userId = window.localStorage.getItem('GameId')
-                console.log(userId)
+                router.push('Join')
             }
          }).catch((error)=>{ 
             console.error(error)
@@ -50,30 +55,20 @@ function App() {
         })    
     }
 
-    useEffect(()=>{
-        let userId = account.title 
-        setUser(userId)
-        setAdd(userId)
-        let room = window.localStorage.getItem('GameRoom')
-        const usersRef = ref(rdb, `${room}/`+'time')
-        onChildAdded(usersRef,(snapshot)=>{
-            let time = Object.entries(snapshot.val())[0]
-            console.log(time[1])
-            window.localStorage.setItem('Timer', time[1])
-            router.push('/Game')
-        })
-    },[])
 
 return(
     <div className="center column" style={{height:"100vh"}}>       
-        {add ? <div className="double WaitScreen" >Waiting for host</div> :<div>
-            <div className="double center">{user} { user && <button onClick={AddList} className="green" style={{fontSize:"20px",margin:"10px",fontWeight:"bold",padding:"8px",borderRadius:"18px"}} >Ready</button>}</div>
-            <br></br>
+        {user ? <div className="double" >
             <div>
+            <div className="double center">{user} { add && <button onClick={AddList} className="green" style={{fontSize:"20px",margin:"10px",fontWeight:"bold",padding:"8px",borderRadius:"18px"}} >Ready</button>}</div>
+                {!add && <input placeholder="Code" value={code.title} type='text' onChange = {(e) => setCode({...code, title: e.target.value})} ></input>}
+                { !add && <button onClick={()=>{inList()}} >Enter</button>}
+            </div> 
+        </div> 
+            :<div>
                 <input placeholder="Name" value={account.title} type='text' onChange = {(e) => setAccount({...account, title: e.target.value})} ></input>
-                <button onClick={()=>{setUser(account.title)}} >Enter</button>
-            </div>    
-        </div>}
+                <button onClick={()=>{setUser(account.title);console.log(account.title)}} >Enter</button>
+            </div>}
     </div>
 )
 
