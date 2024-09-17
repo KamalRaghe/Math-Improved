30b9bcd3e6ddeffae08a0c1f8b21d16ba69e007d
Kamal <kamal.raghe33@gmail.com>
2024-09-17 19:17:37 -0400

    commit

--- pages/Host.js
+++ pages/Host.js
@@ -25,11 +25,21 @@ function App() {
    
 
     useEffect(()=>{
-        let userId = window.localStorage.getItem('GameId')
-        if(!userId){
+        let room = window.localStorage.getItem('GameRoom')
+        setUser(room)
+        if(room){
+            const roomRef = ref(rdb, `${room}/`)
+            get(roomRef).then((snapshot)=>{
+                if(!(snapshot.exists())){
+                    router.push('/create')
+                }
+            }).catch((error)=>{ 
+                console.error(error)
+            
+            })
+        }else{
             router.push('/create')
         }
-        let room = window.localStorage.getItem('GameRoom')
         const usersRef = ref(rdb, `${room}/`+'time')
         onChildAdded(usersRef,(snapshot)=>{
             let time = Object.entries(snapshot.val())[0]
@@ -40,7 +50,8 @@ function App() {
     },[])
 
 return(
-    <div className="center column" style={{height:"100vh"}}>       
+    <div className="center column" style={{height:"100vh"}}>
+        {<div className="double" style={{width:"80%",height:"50%"}}>Code: {user}</div>}       
         <button onClick={Timer} className="green" style={{fontSize:"20px",margin:"10px",fontWeight:"bold",padding:"8px",borderRadius:"18px"}} >Start</button>        
     </div>
 )
--- pages/Join.js
+++ pages/Join.js
@@ -13,46 +13,19 @@ function App() {
     title:""
     })
 
-    function updateList(){
-        set(ref(rdb, 'users/' + '3222/' + '-O68N6PLEqfBXLJrFPLv'),{
-            title:account.title,
-        }).then(()=>{
-            console.log('done Added to list ')
-        })
-    }
-
-    function AddList(){
-        setAdd(true)
-        const usersRef = ref(rdb, 'users/')
-        const AddList = push(usersRef)
-        set(AddList,{
-            user: account.title
-        })
-        get(usersRef).then((snapshot)=>{
-            if(snapshot.exists()){
-                const usersArray = Object.entries(snapshot.val()).map(([id,data])=>({
-                    id,
-                    data,
-                }))
-            
-                for(let i = 0; i < usersArray.length;i++){
-                    if(usersArray[i].data.user === account.title){
-                        window.localStorage.setItem('GameId',usersArray[i].id)
-                        window.localStorage.setItem('GameName',usersArray[i].data.user)
-                    }
-                }
-                let userId = window.localStorage.getItem('GameId')
-                console.log(userId)
-            }
-         }).catch((error)=>{ 
-            console.error(error)
-        
-        })    
-    }
-
     useEffect(()=>{
         let room = window.localStorage.getItem('GameRoom')
-        if(!room){
+        if(room){
+            const roomRef = ref(rdb, `${room}/`)
+            get(roomRef).then((snapshot)=>{
+                if(!(snapshot.exists())){
+                    router.push('/Enter')
+                }
+            }).catch((error)=>{ 
+                console.error(error)
+            
+            })
+        }else{
             router.push('/Enter')
         }
         const usersRef = ref(rdb, `${room}/`+'time')
@@ -66,14 +39,7 @@ function App() {
 
 return(
     <div className="center column" style={{height:"100vh"}}>       
-        {add ? <div className="double WaitScreen" >Waiting for host</div> :<div>
-            <div className="double center">{user} { user && <button onClick={AddList} className="green" style={{fontSize:"20px",margin:"10px",fontWeight:"bold",padding:"8px",borderRadius:"18px"}} >Ready</button>}</div>
-            <br></br>
-            <div>
-                <input placeholder="Name" value={account.title} type='text' onChange = {(e) => setAccount({...account, title: e.target.value})} ></input>
-                <button onClick={()=>{setUser(account.title)}} >Enter</button>
-            </div>    
-        </div>}
+         <div className="double WaitScreen" >Waiting for host</div>
     </div>
 )
 
