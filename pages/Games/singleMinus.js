5c0832f6f938395c06b1abc95b6449bd2593c42c
Kamal <kamal.raghe33@gmail.com>
2024-09-20 19:29:39 -0400

    commit

--- pages/Games/singleMinus.js
+++ pages/Games/singleMinus.js
@@ -8,6 +8,9 @@ import { useRouter } from "next/router";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
 import axios from "axios";
+import { set, ref } from "firebase/database";
+import { rdb } from "@/firebase";
+
 
 export default function DoubleAdd(){
 
@@ -20,12 +23,21 @@ export default function DoubleAdd(){
     const [num2, setNum2] = useState(Math.ceil(Math.random()*9));
     const [num3, setNum3] = useState([0,1,-1,Math.ceil(Math.random()*2+1),-1*Math.ceil(Math.random()*2+1)])
     const router = useRouter()
+    const [score, setScore] =useState(0)
+    const [name, setName] =useState()
+    const [time, setTime] = useState()
+    const [start, setStart] = useState(3)
+    const [date, setDate] = useState(Date.now())
     const {username} = router.query 
     const {id} = router.query 
 
     function mix(){
         setNum3([0,1,-1,Math.ceil(Math.random()*2+1),-1*Math.ceil(Math.random()*2+1)])
     }
+
+    function update(){
+        setDate(requestAnimationFrame(update))
+      }
 
     function open(){
         setHelp(true)
@@ -36,12 +48,11 @@ export default function DoubleAdd(){
 
     function CorrectA(){ 
         setCorrect(true)
-        setCount(count + 1)
         setScore(score+1)
         setTimeout(() => {
             setCorrect(false)
             setReady(true) 
-        }, 1900);
+        }, 1200);
       }
       
       function WrongA(){ 
@@ -49,7 +60,7 @@ export default function DoubleAdd(){
         setTimeout(() => {
             setReady(true)
             setWrong(false) 
-        }, 1900);
+        }, 1200);
       } 
     function Add(){
         setTimeout(() => {
@@ -57,74 +68,71 @@ export default function DoubleAdd(){
             setNum2(Math.ceil(Math.random()*9))
             mix()
             setNum3(prevChange => prevChange.sort((a,b)=>Math.random()-0.5)) 
-        }, 1900)
+        }, 1200)
     }
 
-    const [score, setScore] =useState(0)
-    const [count, setCount] =useState(0)
+    function updateList(){
+        let name = window.localStorage.getItem('GameName')
+        let id = window.localStorage.getItem('GameId')
+        let room = window.localStorage.getItem('GameRoom')
+        set(ref(rdb, `${room}/` + id),{
+            user: name,
+            score: score
+        }).then(()=>{
+            router.push('/Score')
+        })
+    }
 
+    
     useEffect(() =>{
-        setLoaded(true)
-        const count = parseInt(window.localStorage.getItem(`singleAdd ${id}`))
-        setCount(count ? count : 0)
-        const score = parseInt(window.localStorage.getItem(`${id} score`))
-        setScore(score ? score : 0)
+        let timer = window.localStorage.getItem('Timer')
+        setTime(timer)
+        setTimeout(() => {
+            setStart(2)
+        }, 1000); 
+        setTimeout(() => {
+            setStart(1)
+        }, 2000);
+        setTimeout(() => {
+            setStart()
+            setLoaded(true)
+            update() 
+        }, 3000);
+        let name = window.localStorage.getItem('GameName')
+        setName(name)
     },[])
-
-    useEffect(() =>{
-        if(count > 0){
-        window.localStorage.setItem(`singleAdd ${id}`, count)
-    }},[count])
-
-    useEffect(() =>{
-        if(score > 0){
-        window.localStorage.setItem(`${id} score` , score)
-    }},[score])
-
 
     useEffect(() =>{
         mix()
         setNum3(prevChange => prevChange.sort((a,b)=>Math.random()-0.5))
      },[num1])
 
-     useEffect(()=>{
-        const ID = window.localStorage.getItem('ID')
-        if(!(ID === id)){
-            router.push("/")
+     useEffect(() =>{
+        if(time - Date.now() < 0 ){
+            updateList()
         }
-    },[])
+    })
 
     return(
-        <div className="beige container column">
-            <div className="Test sb"><div className="double" >
-                <div>Score: {loaded && score}</div>
-                <div className="font" >Single digit Addition: {loaded && count} </div>
+        <div className="beige container column" >
+            <div className="Test sb" style={{alignItems:"end"}}><div className="double" >
+                {loaded && <div><div>Score: {score}</div><div style={{fontSize:"20px"}} >{name}</div></div>}
             </div>
-                <Link href={`/${id}/enter/testSingleAdd`}>
-                    <button className="green test-btn">Test</button>
-                </Link>
-            </div>
-            {ready ? <div className="center " style={{fontSize:"30px",width:"320px",marginTop:"10px"}}>
                 <div>
-                    <div>{num1} + 0 = {num1+0}</div>
-                    <div>{num1} + 1 = {num1+1}</div>
-                    <div>{num1} + 2 = {num1+2}</div>
-                    <div>{num1} + 3 = {num1+3}</div>
-                    <div>{num1} + 4 = {num1+4}</div>
-                    <div>{num1} + 5 = {num1+5}</div>
-                    <div>{num1} + 6 = {num1+6}</div>
-                    <div>{num1} + 7 = {num1+7}</div>
-                    <div>{num1} + 8 = {num1+8}</div>
-                    <div>{num1} + 9 = {num1+9}</div>
-                </div>
-                <button className="green choice" onClick={() => setReady(false)} style={{borderRadius:"20px",margin:"10px"}} >Ready</button>
-            </div>:<div className="center column" >
-                <div className="box column">
-                    <div className="double">{loaded && num1} + {loaded && num2} =</div>
+                    {loaded && time-Date.now() > 60 && <span style={{fontSize:"30px",padding:"5px"}}>{Math.floor(((time-Date.now())%(1000*60*60))/60000)}m</span>}
+                    {loaded && time-Date.now() > 0 && <span style={{fontSize:"30px"}}>{Math.floor(((time-Date.now())%(1000*60))/1000)}s</span>}
+                </div>    
+            </div>
+            {<div className="center column" >
+                <div className="box column" style={{width:'300px'}}>
+                    {loaded && <div className="double">{num1} + {num2} =
+                        {loaded && correct && <span className="Green" style={{padding:"10px"}} >{num1+num2}</span>}
+                        {loaded && wrong && <span className="Red" style={{padding:"10px"}} >{num1+num2}</span>}
+                    </div>}
                 </div>
                 {help && <HelpAdd num1 ={num1} num2={num2} close={close}/>}
-                {loaded && correct && <Correct></Correct>}
-                {loaded && wrong && <Wrong/> }
+                {<div className="countStart" >{start}</div>}
+                {loaded && <button className="help" style={{zIndex:"20"}} onClick={open}>help</button>}
                 <div style={{height:"30px"}} ></div>
                 <div className="box column">
                 <div className="row ">
