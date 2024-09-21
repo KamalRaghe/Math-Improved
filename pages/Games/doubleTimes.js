1165f574bcb563303233add9dc55cd21cf5046d4
Kamal <kamal.raghe33@gmail.com>
2024-09-20 20:17:39 -0400

    commit

--- pages/Games/doubleTimes.js
+++ pages/Games/doubleTimes.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Choice from "@/components/choice";
 import Correct from "@/components/correct";
 import Wrong from "@/components/wrong"; 
-import StepAdd from '@/components/StepAdd'
+import StepTimes from '@/components/stepTimes'
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
@@ -15,13 +15,13 @@ export default function DoubleAdd(){
     const[ wrong, setWrong] = useState(false)
     const [num1, setNum1] = useState(Math.floor(Math.random()*90+10));
     const [num2, setNum2] = useState(Math.floor(Math.random()*90+10));
-    const [num3, setNum3] = useState([0,Math.ceil(Math.random()*10),-1*Math.ceil(Math.random()*10),Math.ceil(Math.random()*20+10),-1*(Math.ceil(Math.random()*10+10))])
+    const [num3, setNum3] = useState(([0,Math.ceil(Math.random()*50),-1*Math.ceil(Math.random()*50),Math.ceil(Math.random()*50+50),-1*(Math.ceil(Math.random()*50+50))]))
     const router = useRouter()
     const {username} = router.query 
     const {id} = router.query 
 
     function mix(){
-        setNum3([0,Math.ceil(Math.random()*10),-1*Math.ceil(Math.random()*10),Math.ceil(Math.random()*20+10),-1*(Math.ceil(Math.random()*10+10))])
+        setNum3(([0,Math.ceil(Math.random()*50),-1*Math.ceil(Math.random()*50),Math.ceil(Math.random()*50+50),-1*(Math.ceil(Math.random()*50+50))]))
     }
 
     function open(){
@@ -65,7 +65,7 @@ export default function DoubleAdd(){
  
      useEffect(() =>{
          setLoaded(true)
-         const count = parseInt(window.localStorage.getItem(`${id} DoubleAdd`))
+         const count = parseInt(window.localStorage.getItem(`${id} DoubleTimes`))
          setCount(count ? count : 0)
          const score = parseInt(window.localStorage.getItem(`${id} score`))
          setScore(score ? score : 0)
@@ -77,7 +77,7 @@ export default function DoubleAdd(){
  
      useEffect(() =>{
          if(count > 0){
-         window.localStorage.setItem(`${id} DoubleAdd`, count)
+         window.localStorage.setItem(`${id} DoubleTimes`, count)
      }},[count])
  
      useEffect(() =>{
@@ -90,26 +90,26 @@ export default function DoubleAdd(){
             <div className="Test sb"><div className="double" >
                 <div>Score: {loaded && score}</div>
                 <div className="font" >Double digit Addition: {loaded && count} </div>
-            </div><Link href={`/${id}/enter/testDoubleAdd`}><button className="green test-btn">Test</button></Link></div>
+            </div><Link href={`/${id}/enter/testDoubleTimes`}><button className="green test-btn">Test</button></Link></div>
             <div className="box column">
                 <div className="double top-number">{loaded && num1}</div>
-                <div className="double bottom-number">+{loaded && num2}</div>
+                <div className="double bottom-number">x{loaded && num2}</div>
             </div>
             <div className="box">
                 <button className="help" onClick={open}>Step by step</button>
             </div>
-            {help && <StepAdd num1 ={num1} num2={num2} close={close}/>}
+            {help && <StepTimes num1 ={num1} num2={num2} close={close}/>}
             {loaded && correct && <Correct></Correct>}
             {loaded && wrong && <Wrong/> }
             <div className="box column">
                <div className="row ">
-                    { loaded && <Choice value ={num1+num2+num3[0]} answer ={num1+num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && <Choice value ={num1+num2+num3[1]} answer ={num1+num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && <Choice value ={num1+num2+num3[2]} answer ={num1+num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice value ={num1*num2+num3[0]} answer ={num1*num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice value ={num1*num2+num3[1]} answer ={num1*num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice value ={num1*num2+num3[2]} answer ={num1*num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
                </div>
                <div className="row">
-                    { loaded && <Choice value ={num1+num2+num3[3]} answer ={num1+num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && <Choice value ={num1+num2+num3[4]} answer ={num1+num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice value ={num1*num2+num3[3]} answer ={num1*num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice value ={num1*num2+num3[4]} answer ={num1*num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
                </div>
             </div>
         </div>
