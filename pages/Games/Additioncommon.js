80fc4fdd96dd9ceb5b1624688eea266f53271e40
Kamal <kamal.raghe33@gmail.com>
2024-09-22 21:23:19 -0400

    commit

--- pages/Games/Additioncommon.js
+++ pages/Games/Additioncommon.js
@@ -2,19 +2,20 @@ import { useEffect, useState } from "react";
 import Choice from "@/components/choice";
 import Correct from "@/components/correct";
 import Wrong from "@/components/wrong"; 
+import HelpMinus from '@/components/HelpMinus'
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
-import LongDivisionHelp from "@/components/longDivisionHelp";
 
 export default function DoubleAdd(){
+
     const [help, setHelp] = useState(false)
     const [loaded, setLoaded] = useState(false)
     const [correct, setCorrect] = useState(false)
     const[ wrong, setWrong] = useState(false)
-    const [num1, setNum1] = useState(Math.ceil(Math.random()*8+1));
-    const [num2, setNum2] = useState(Math.ceil(Math.random()*900 + 99));
+    const [num1, setNum1] = useState(Math.ceil(Math.random()*4+5));
+    const [num2, setNum2] = useState(Math.ceil(Math.random()*4));
     const [num3, setNum3] = useState([0,1,-1,Math.ceil(Math.random()*2+1),-1*Math.ceil(Math.random()*2+1)])
     const router = useRouter()
     const {username} = router.query 
@@ -33,12 +34,16 @@ export default function DoubleAdd(){
 
     function CorrectA(){ 
         setCorrect(true)
+        setCount(count+1)
+        setScore(score+1)
         setTimeout(() => {
             setCorrect(false) 
         }, 1900);
-        setCount(count+1)
-        setScore(score+1)
-    }
+        setCorrect(true)
+        setTimeout(() => {
+            setCorrect(false) 
+        }, 1900);
+      }
   
       function WrongA(){ 
         setWrong(true)
@@ -48,77 +53,78 @@ export default function DoubleAdd(){
       } 
     function Add(){
         setTimeout(() => {
-            setNum1(Math.ceil(Math.random()*8+1))
-            setNum2(Math.ceil(Math.random()*900+99))   
+            setNum1(Math.ceil(Math.random()*9))
+            
             mix()
             setNum3(prevChange => prevChange.sort((a,b)=>Math.random()-0.5)) 
         }, 1500)
     }
 
+    const [score, setScore] =useState(0)
+    const [count, setCount] =useState(0)
+
+    useEffect(() =>{
+        setLoaded(true)
+        const count = parseInt(window.localStorage.getItem(`${id} singleMinus`))
+        setCount(count ? count : 0)
+        const score = parseInt(window.localStorage.getItem(`${id} score`))
+        setScore(score ? score : 0)
+    },[])
+
+    useEffect(() =>{
+        if(count > 0){
+        window.localStorage.setItem(`${id} singleMinus`, count)
+    }},[count])
+
+    useEffect(() =>{
+        if(score > 0){
+        window.localStorage.setItem(`${id} score` , score)
+    }},[score])
 
     useEffect(() =>{
         mix()
         setNum3(prevChange => prevChange.sort((a,b)=>Math.random()-0.5))
+        setNum2(Math.ceil(Math.random()*(num1-1)))
      },[num1])
 
-     const [score, setScore] =useState(0)
-     const [count, setCount] =useState(0)
- 
-     useEffect(() =>{
-         setLoaded(true)
-         const count = parseInt(window.localStorage.getItem(`${id} longDivision`))
-         setCount(count ? count : 0)
-         const score = parseInt(window.localStorage.getItem(`${id} score`))
-         setScore(score ? score : 0)
-         const ID = window.localStorage.getItem('ID')
+    useEffect(() =>{
+        setLoaded(true)
+    },[])
+
+    useEffect(()=>{
+        const ID = window.localStorage.getItem('ID')
         if(!(ID === id)){
             router.push("/")
         }
-     },[])
- 
-     useEffect(() =>{
-         if(count > 0){
-         window.localStorage.setItem(`${id} longDivision`, count)
-     }},[count])
- 
-     useEffect(() =>{
-         if(score > 0){
-         window.localStorage.setItem(`${id} score` , score)
-     }},[score])
+    },[])
 
     return(
         <div className="beige container column">
-            <div className="Test sb"><div className="double" >
+             <div className="Test sb"><div className="double" >
                 <div>Score: {loaded && score}</div>
-                <div className="font" >Long Division: {loaded && count} </div>
-            </div><Link href={`/${id}/enter/testLongDivision`}><button className="green test-btn">Test</button></Link></div>
-            <div className="box">
-                <div className="double center">{loaded && num1}<div style={{borderLeft: '3px solid black', borderTop: '3px solid black', marginLeft:'5px', paddingRight:'10px'}}><span className="hide">.</span>{loaded && num2}</div> </div>
+                <div className="font" >Single digit Subtraction: {loaded && count} </div>
+            </div>
+            <Link href={`/${id}/enter/testSingleMinus`}>
+                <button className="green test-btn">Test</button>
+            </Link></div>
+            <div className="box column">
+                <div className="double">{loaded && num1} - {loaded && num2} =</div>
             </div>
             <div className="box">
                 <button className="help" onClick={open}>help</button>
             </div>
-            {help && <LongDivisionHelp num1 ={num1} num2={num2} close={close}/>}
+            {/* {help && <HelpMinus num1 ={num1} num2={num2} close={close}/>} */}
             {loaded && correct && <Correct></Correct>}
             {loaded && wrong && <Wrong/> }
             <div className="box column">
                <div className="row ">
-                    { loaded && num2 % num1 > 0 && <Choice big={true} size={'65px'}  value ={`${((num2-(num2 % num1))/num1)-num3[0]} R${(num2%num1)}`} answer ={`${((num2-(num2 % num1))/num1)} R${(num2%num1)}`} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && num2 % num1 > 0 && <Choice big={true} size={'65px'}  value ={`${((num2-(num2 % num1))/num1)-num3[1]} R${(num2%num1)}`} answer ={`${((num2-(num2 % num1))/num1)} R${(num2%num1)}`} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && num2 % num1 > 0 && <Choice big={true} size={'65px'}  value ={`${((num2-(num2 % num1))/num1)-num3[2]} R${(num2%num1)}`} answer ={`${((num2-(num2 % num1))/num1)} R${(num2%num1)}`} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice value ={num1-num2+num3[0]} answer ={num1-num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice value ={num1-num2+num3[1]} answer ={num1-num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice value ={num1-num2+num3[2]} answer ={num1-num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
                </div>
                <div className="row">
-                    { loaded && num2 % num1 > 0 && <Choice big={true} size={'65px'}  value ={`${((num2-(num2 % num1))/num1)-num3[3]} R${(num2%num1)}`} answer ={`${((num2-(num2 % num1))/num1)} R${(num2%num1)}`} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && num2 % num1 > 0 && <Choice big={true} size={'65px'}  value ={`${((num2-(num2 % num1))/num1)-num3[4]} R${(num2%num1)}`} answer ={`${((num2-(num2 % num1))/num1)} R${(num2%num1)}`} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-               </div>
-               <div className="row">
-                    { loaded && num2 % num1 === 0 && <Choice  value ={((num2-(num2 % num1))/num1)-num3[0]} answer ={((num2-(num2 % num1))/num1)} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && num2 % num1 === 0 && <Choice  value ={((num2-(num2 % num1))/num1)-num3[1]} answer ={((num2-(num2 % num1))/num1)} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && num2 % num1 === 0 && <Choice  value ={((num2-(num2 % num1))/num1)-num3[2]} answer ={((num2-(num2 % num1))/num1)} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-               </div>
-               <div className="row">
-                    { loaded && num2 % num1 === 0 && <Choice  value ={((num2-(num2 % num1))/num1)-num3[3]} answer ={((num2-(num2 % num1))/num1)} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && num2 % num1 === 0 && <Choice  value ={((num2-(num2 % num1))/num1)-num3[4]} answer ={((num2-(num2 % num1))/num1)} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice value ={num1-num2+num3[3]} answer ={num1-num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice value ={num1-num2+num3[4]} answer ={num1-num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
                </div>
             </div>
         </div>
