5b413a3a06051a324e3e8e42dc6a8fc1420d99f5
Kamal <kamal.raghe33@gmail.com>
2024-09-22 21:22:23 -0400

    commit

--- pages/Games/Additioncommon.js
+++ pages/Games/Additioncommon.js
@@ -2,42 +2,27 @@ import { useEffect, useState } from "react";
 import Choice from "@/components/choice";
 import Correct from "@/components/correct";
 import Wrong from "@/components/wrong"; 
-import HelpAdd from '@/components/HelpAdd'
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
-import axios from "axios";
-import { set, ref } from "firebase/database";
-import { rdb } from "@/firebase";
-
+import LongDivisionHelp from "@/components/longDivisionHelp";
 
 export default function DoubleAdd(){
-
     const [help, setHelp] = useState(false)
     const [loaded, setLoaded] = useState(false)
-    const [ready, setReady] = useState(true)
     const [correct, setCorrect] = useState(false)
     const[ wrong, setWrong] = useState(false)
-    const [num1, setNum1] = useState(Math.ceil(Math.random()*9));
-    const [num2, setNum2] = useState(Math.ceil(Math.random()*9));
+    const [num1, setNum1] = useState(Math.ceil(Math.random()*8+1));
+    const [num2, setNum2] = useState(Math.ceil(Math.random()*900 + 99));
     const [num3, setNum3] = useState([0,1,-1,Math.ceil(Math.random()*2+1),-1*Math.ceil(Math.random()*2+1)])
     const router = useRouter()
-    const [score, setScore] =useState(0)
-    const [name, setName] =useState()
-    const [time, setTime] = useState()
-    const [start, setStart] = useState(3)
-    const [date, setDate] = useState(Date.now())
     const {username} = router.query 
     const {id} = router.query 
 
     function mix(){
         setNum3([0,1,-1,Math.ceil(Math.random()*2+1),-1*Math.ceil(Math.random()*2+1)])
     }
-
-    function update(){
-        setDate(requestAnimationFrame(update))
-      }
 
     function open(){
         setHelp(true)
@@ -48,104 +33,94 @@ export default function DoubleAdd(){
 
     function CorrectA(){ 
         setCorrect(true)
-        setScore(score+1)
         setTimeout(() => {
-            setCorrect(false)
-            setReady(true) 
-        }, 1200);
-      }
-      
+            setCorrect(false) 
+        }, 1900);
+        setCount(count+1)
+        setScore(score+1)
+    }
+  
       function WrongA(){ 
         setWrong(true)
         setTimeout(() => {
-            setReady(true)
             setWrong(false) 
-        }, 1200);
+        }, 1900);
       } 
     function Add(){
         setTimeout(() => {
-            setNum1(Math.ceil(Math.random()*9))
-            setNum2(Math.ceil(Math.random()*9))
+            setNum1(Math.ceil(Math.random()*8+1))
+            setNum2(Math.ceil(Math.random()*900+99))   
             mix()
             setNum3(prevChange => prevChange.sort((a,b)=>Math.random()-0.5)) 
-        }, 1200)
+        }, 1500)
     }
 
-    function updateList(){
-        let name = window.localStorage.getItem('GameName')
-        let id = window.localStorage.getItem('GameId')
-        let room = window.localStorage.getItem('GameRoom')
-        set(ref(rdb, `${room}/` + id),{
-            user: name,
-            score: score
-        }).then(()=>{
-            router.push('/Score')
-        })
-    }
-
-    
-    useEffect(() =>{
-        let timer = window.localStorage.getItem('Timer')
-        setTime(timer)
-        setTimeout(() => {
-            setStart(2)
-        }, 1000); 
-        setTimeout(() => {
-            setStart(1)
-        }, 2000);
-        setTimeout(() => {
-            setStart()
-            setLoaded(true)
-            update() 
-        }, 3000);
-        let name = window.localStorage.getItem('GameName')
-        setName(name)
-    },[])
 
     useEffect(() =>{
         mix()
         setNum3(prevChange => prevChange.sort((a,b)=>Math.random()-0.5))
      },[num1])
 
+     const [score, setScore] =useState(0)
+     const [count, setCount] =useState(0)
+ 
      useEffect(() =>{
-        if(time - Date.now() < 0 ){
-            updateList()
+         setLoaded(true)
+         const count = parseInt(window.localStorage.getItem(`${id} longDivision`))
+         setCount(count ? count : 0)
+         const score = parseInt(window.localStorage.getItem(`${id} score`))
+         setScore(score ? score : 0)
+         const ID = window.localStorage.getItem('ID')
+        if(!(ID === id)){
+            router.push("/")
         }
-    })
+     },[])
+ 
+     useEffect(() =>{
+         if(count > 0){
+         window.localStorage.setItem(`${id} longDivision`, count)
+     }},[count])
+ 
+     useEffect(() =>{
+         if(score > 0){
+         window.localStorage.setItem(`${id} score` , score)
+     }},[score])
 
     return(
-        <div className="beige container column" >
-            <div className="Test sb" style={{alignItems:"end"}}><div className="double" >
-                {loaded && <div><div>Score: {score}</div><div style={{fontSize:"20px"}} >{name}</div></div>}
+        <div className="beige container column">
+            <div className="Test sb"><div className="double" >
+                <div>Score: {loaded && score}</div>
+                <div className="font" >Long Division: {loaded && count} </div>
+            </div><Link href={`/${id}/enter/testLongDivision`}><button className="green test-btn">Test</button></Link></div>
+            <div className="box">
+                <div className="double center">{loaded && num1}<div style={{borderLeft: '3px solid black', borderTop: '3px solid black', marginLeft:'5px', paddingRight:'10px'}}><span className="hide">.</span>{loaded && num2}</div> </div>
             </div>
-                <div>
-                    {loaded && time-Date.now() > 60 && <span style={{fontSize:"30px",padding:"5px"}}>{Math.floor(((time-Date.now())%(1000*60*60))/60000)}m</span>}
-                    {loaded && time-Date.now() > 0 && <span style={{fontSize:"30px"}}>{Math.floor(((time-Date.now())%(1000*60))/1000)}s</span>}
-                </div>    
+            <div className="box">
+                <button className="help" onClick={open}>help</button>
             </div>
-            {<div className="center column" >
-                <div className="box column" style={{width:'300px'}}>
-                    {loaded && <div className="double">{num1} + {num2} =
-                        {loaded && correct && <span className="Green" style={{padding:"10px"}} >{num1+num2}</span>}
-                        {loaded && wrong && <span className="Red" style={{padding:"10px"}} >{num1+num2}</span>}
-                    </div>}
-                </div>
-                {help && <HelpAdd num1 ={num1} num2={num2} close={close}/>}
-                {<div className="countStart" >{start}</div>}
-                {loaded && <button className="help" style={{zIndex:"20"}} onClick={open}>help</button>}
-                <div style={{height:"30px"}} ></div>
-                <div className="box column">
-                <div className="row ">
-                        { loaded && <Choice value ={num1+num2+num3[0]} answer ={num1+num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                        { loaded && <Choice value ={num1+num2+num3[1]} answer ={num1+num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                        { loaded && <Choice value ={num1+num2+num3[2]} answer ={num1+num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                </div>
-                <div className="row">
-                        { loaded && <Choice value ={num1+num2+num3[3]} answer ={num1+num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                        { loaded && <Choice value ={num1+num2+num3[4]} answer ={num1+num2} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                </div>
-                </div>
-            </div>}
+            {help && <LongDivisionHelp num1 ={num1} num2={num2} close={close}/>}
+            {loaded && correct && <Correct></Correct>}
+            {loaded && wrong && <Wrong/> }
+            <div className="box column">
+               <div className="row ">
+                    { loaded && num2 % num1 > 0 && <Choice big={true} size={'65px'}  value ={`${((num2-(num2 % num1))/num1)-num3[0]} R${(num2%num1)}`} answer ={`${((num2-(num2 % num1))/num1)} R${(num2%num1)}`} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && num2 % num1 > 0 && <Choice big={true} size={'65px'}  value ={`${((num2-(num2 % num1))/num1)-num3[1]} R${(num2%num1)}`} answer ={`${((num2-(num2 % num1))/num1)} R${(num2%num1)}`} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && num2 % num1 > 0 && <Choice big={true} size={'65px'}  value ={`${((num2-(num2 % num1))/num1)-num3[2]} R${(num2%num1)}`} answer ={`${((num2-(num2 % num1))/num1)} R${(num2%num1)}`} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+               </div>
+               <div className="row">
+                    { loaded && num2 % num1 > 0 && <Choice big={true} size={'65px'}  value ={`${((num2-(num2 % num1))/num1)-num3[3]} R${(num2%num1)}`} answer ={`${((num2-(num2 % num1))/num1)} R${(num2%num1)}`} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && num2 % num1 > 0 && <Choice big={true} size={'65px'}  value ={`${((num2-(num2 % num1))/num1)-num3[4]} R${(num2%num1)}`} answer ={`${((num2-(num2 % num1))/num1)} R${(num2%num1)}`} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+               </div>
+               <div className="row">
+                    { loaded && num2 % num1 === 0 && <Choice  value ={((num2-(num2 % num1))/num1)-num3[0]} answer ={((num2-(num2 % num1))/num1)} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && num2 % num1 === 0 && <Choice  value ={((num2-(num2 % num1))/num1)-num3[1]} answer ={((num2-(num2 % num1))/num1)} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && num2 % num1 === 0 && <Choice  value ={((num2-(num2 % num1))/num1)-num3[2]} answer ={((num2-(num2 % num1))/num1)} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+               </div>
+               <div className="row">
+                    { loaded && num2 % num1 === 0 && <Choice  value ={((num2-(num2 % num1))/num1)-num3[3]} answer ={((num2-(num2 % num1))/num1)} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && num2 % num1 === 0 && <Choice  value ={((num2-(num2 % num1))/num1)-num3[4]} answer ={((num2-(num2 % num1))/num1)} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+               </div>
+            </div>
         </div>
     )
 }
