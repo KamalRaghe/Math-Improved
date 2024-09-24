0893ae5885753c7f6aa6d624f357b3175e6b7b0f
Kamal <kamal.raghe33@gmail.com>
2024-09-24 14:47:59 -0400

    commit

--- pages/Games/Doubledigitsubtraction.js
+++ pages/Games/Doubledigitsubtraction.js
@@ -5,8 +5,8 @@ import Wrong from "@/components/wrong";
 import StepMinus from '@/components/StepMinus'
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
-import { db } from "@/firebase";
+import { set, ref } from "firebase/database";
+import { rdb } from "@/firebase";
 
 export default function DoubleAdd(){
     const [help, setHelp] = useState(false)
@@ -19,6 +19,51 @@ export default function DoubleAdd(){
     const router = useRouter()
     const {username} = router.query 
     const {id} = router.query 
+    const [name, setName] =useState()
+    const [time, setTime] = useState()
+    const [start, setStart] = useState(3)
+    const [date, setDate] = useState(Date.now())
+
+    function update(){
+        setDate(requestAnimationFrame(update))
+      }
+
+    function updateList(){
+        let name = window.localStorage.getItem('GameName')
+        let id = window.localStorage.getItem('GameId')
+        let room = window.localStorage.getItem('GameRoom')
+        set(ref(rdb, `${room}/` + id),{
+            user: name,
+            score: score
+        }).then(()=>{
+            router.push('/Score')
+        })
+    }
+
+    
+    useEffect(() =>{
+        let timer = window.localStorage.getItem('Timer')
+        setTime(timer)
+        setTimeout(() => {
+            setStart(2)
+        }, 1000); 
+        setTimeout(() => {
+            setStart(1)
+        }, 2000);
+        setTimeout(() => {
+            setStart()
+            setLoaded(true)
+            update() 
+        }, 3000);
+        let name = window.localStorage.getItem('GameName')
+        setName(name)
+    },[])
+
+     useEffect(() =>{
+        if(time - Date.now() < 0 ){
+            updateList()
+        }
+    })
 
     function mix(){
         setNum3([0,Math.ceil(Math.random()*10),-1*Math.ceil(Math.random()*10),Math.ceil(Math.random()*20+10),-1*(Math.ceil(Math.random()*10+10))])
@@ -33,7 +78,6 @@ export default function DoubleAdd(){
 
     function CorrectA(){ 
         setCorrect(true)
-        setCount(count+1)
         setScore(score+1)
         setTimeout(() => {
             setCorrect(false) 
@@ -61,40 +105,24 @@ export default function DoubleAdd(){
      },[num1])
 
      const [score, setScore] =useState(0)
-     const [count, setCount] =useState(0)
- 
-     useEffect(() =>{
-        setLoaded(true)
-         const count = parseInt(window.localStorage.getItem(`${id} DoubleMinus`))
-         setCount(count ? count : 0)
-         const score = parseInt(window.localStorage.getItem(`${id} score`))
-         setScore(score ? score : 0)
-         const ID = window.localStorage.getItem('ID')
-        if(!(ID === id)){
-            router.push("/")
-        }
-     },[])
- 
-     useEffect(() =>{
-         if(count > 0){
-         window.localStorage.setItem(`${id} DoubleMinus`, count)
-        
-     }},[count])
- 
-     useEffect(() =>{
-         if(score > 0){
-         window.localStorage.setItem(`${id} score` , score)
-     }},[score])
+    
 
     return(
         <div className="beige container column">
-            <div className="Test sb"><div className="double" >
-                <div>Score: {loaded && score}</div>
-                <div className="font" >Double digit Subtraction: {loaded && count} </div>
-            </div><Link href={`/${id}/enter/testDoubleMinus`}><button className="green test-btn">Test</button></Link></div>
+            <div className="Test sb" style={{alignItems:"end"}}><div className="double" >
+                {loaded && <div><div>Score: {score}</div><div style={{fontSize:"20px"}} >{name}</div></div>}
+            </div>
+                <div>
+                    {loaded && time-Date.now() > 60 && <span style={{fontSize:"30px",padding:"5px"}}>{Math.floor(((time-Date.now())%(1000*60*60))/60000)}m</span>}
+                    {loaded && time-Date.now() > 0 && <span style={{fontSize:"30px"}}>{Math.floor(((time-Date.now())%(1000*60))/1000)}s</span>}
+                </div>    
+            </div>
+                {<div className="countStart" >{start}</div>}
             <div className="box column">
                 <div className="double top-number">{loaded && num1}</div>
                 <div className="double bottom-number">-<span className="hide">.</span>{loaded && num2}</div>
+                {correct && <div className="Green double lower-number">{loaded && num1-num2}</div>}
+                {wrong && <div className="Red double lower-number``">{loaded && num2+num1}</div>}
             </div>
             <div className="box">
                 <button className="help" onClick={open}>Step by step</button>
