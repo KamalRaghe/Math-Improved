932cf26d552c129137ee4bc68bae220a4e67ad5e
Kamal <kamal.raghe33@gmail.com>
2024-09-24 16:27:55 -0400

    commit

--- pages/Host.js
+++ pages/Host.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { rdb } from "@/firebase";
-import { get, ref, push, set, onChildAdded } from "firebase/database";
+import { get, ref, push, set, onChildAdded , remove} from "firebase/database";
 import { useRouter } from "next/router";
 
 
@@ -27,6 +27,17 @@ function App() {
         if(topic){
             setTopic(topic)
         }
+    }
+
+    function Leave(){
+        let room = window.localStorage.getItem('GameRoom')
+        const usersRef = ref(rdb, `${room}/`+'cancel')
+        const AddList = push(usersRef)
+        set(AddList,{
+            time: 63000 + Date.now()
+        }).then(remove(ref(rdb, `${room}/`)))  
+        
+             
     }
 
     useEffect(()=>{
@@ -79,8 +90,9 @@ return(
         </div>
         <br></br>
         <div><button className="sub-topic" style={{margin:"2px"}} onClick={()=>{router.push('/GameMenu')}} >{topic}</button></div>
-        <div className="double" style={{width:"340px",height:"10%",display:"flex",alignItems:"end",}}>
-            <button className="topic red" style={{fontSize:"20px",margin:"10px",fontWeight:"bold",padding:"8px 20px"}}>Leave</button>
+        <div className="double" style={{width:"340px",height:"10%",display:"flex",justifyContent:"space-between",alignItems:"end",}}>
+            <button  onClick={Leave} className="topic red" style={{fontSize:"20px",margin:"10px",fontWeight:"bold",padding:"8px 20px"}}>Leave</button>
+            <button onClick={() => router.push('/GameMenu')}  className="topic" style={{fontSize:"20px",margin:"10px",fontWeight:"bold",borderColor:"red",padding:"8px 20px",backgroundColor:'grey'}}>Topic</button>
         </div>       
         <button onClick={Timer} className="topic green" style={{fontSize:"20px",margin:"10px",fontWeight:"bold",padding:"8px 20px"}} >Start</button>        
     </div>
