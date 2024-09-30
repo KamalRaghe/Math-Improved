32fed853ce0193d66aafa4e22b513a240adeb926
Kamal <kamal.raghe33@gmail.com>
2024-09-30 17:34:36 -0400

    commit

---
+++ pages/Sign.js
@@ -0,0 +1,165 @@
+
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import { auth } from "@/firebase"
+import { db } from "@/firebase"
+import { addDoc, collection } from "firebase/firestore"
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth"
+
+export default function Home() {
+  const [sign, setSign] = useState()
+  const [create, setCreate] = useState(false)
+  const [user, setUser] = useState()
+  const [red, setRed] = useState()
+  const [ loaded, setLoaded] = useState(false)
+  const [password, setPassword] = useState('password')
+  const [move, setMove] = useState('165px')
+  const [id, setId] = useState('id')
+  const [link, setLink] = useState(false)
+  const [account, setAccount] = useState({
+    title:"", 
+    password:""
+    })
+
+  const router = useRouter()
+
+
+
+  function signIn(){
+    const email = account.title
+    const password = account.password
+    signInWithEmailAndPassword(auth,email,password).then((result)=>{
+      setUser(true)
+      router.push('/login')
+    }).catch(err =>{
+      alert(err)
+    })
+  }
+  
+  function Forgot(){
+    const email = account.title
+    if(email){
+      sendPasswordResetEmail(auth, email).then(()=>{alert('Check your email')})
+    }else{
+      alert('Put in your email') 
+    }
+  }
+
+  function CreateUser(e){
+    e.preventDefault()
+    const email = e.target.email.value
+    const password = e.target.password.value
+
+    createUserWithEmailAndPassword(auth,email,password).then(()=>{
+      setUser(true)
+      router.push('/login')
+    }).catch(err =>{
+      alert(err)
+    })
+}
+
+  function Password(){
+    if(password === 'password'){
+      setPassword('')
+      setMove('169px')
+    }else{
+      setPassword('password')
+      setMove('165px')
+    }
+  }
+
+  useEffect(()=>{
+    const SignInUser = window.localStorage.getItem('User')
+    setUser((SignInUser))
+    setLoaded(true)
+    const security = window.localStorage.getItem('Id')
+    if(security){
+      setLink(security)
+    }else{
+      const num = Math.ceil(Math.random()*100000000000000000000)
+      window.localStorage.setItem('Id' , num)
+      setLink(num)
+    }
+  },[])
+
+  useEffect(()=>{
+    if(user === true){
+    window.localStorage.setItem('User', `${auth.currentUser?.email}`)
+    window.localStorage.setItem('uid', `${auth.currentUser?.uid}`)
+    router.push('/login')
+}},[user])
+
+  return (
+    <div className="center beige column">
+      <div className="center column" style={{alignItems:"start"}}>
+        <div className="relative" style={{fontSize:"70px",top:"20px",color:'navy',padding:"0px"}} >Math</div>
+        <div className="relative" style={{fontSize:"70px",color:'purple',paddingBottom:"20px"}} >Improve</div>
+        <div className="relative">
+          {!user && !create && loaded && <div style={{ display:"flex",flexDirection:"column",alignItems:"center", height:"215px",border:"2px solid black",padding:"5px"}}>
+            <div style={{paddingBottom:'10px',fontSize:"50px"}} >
+            <button style={{background:'none',fontSize:"20px",border:'none',color:"blue",display:"flex",justifyContent:"end",width:"110%",paddingTop:"5px"}} onClick={()=>{setCreate(true)}} >Sign up</button>
+              <div className="center" style={{fontSize:"50px",fontWeight:"bold"}}  >Sign In</div> 
+            </div>  
+             
+            <div className="" style={{width:"200px",backgroundColor:"transparent",border:'none'}} >
+              <input className="relative" style={{borderColor: red,width:'190px',marginBottom:"10px"}} name='email' type='email' placeholder="Email" value={account.title} onChange = {(e) => setAccount({...account, title: e.target.value})} ></input>
+              <input style={{borderColor: red,width:'190px',zIndex:"100"}} name='password' type={password} placeholder="password" value={account.password} onChange = {(e) => setAccount({...account, password: e.target.value})} ></input> </div>
+              {!user && !create && loaded && <button style={{position:"relative",left:"43px"}} onClick={Password}>
+              <div className="relative">{password === 'password' ? 'show password' : 'hide password'}</div></button>}
+          <div className="center column" >
+          <button style={{position:'relative',right:"70px"}} onClick={signIn} >Enter</button>
+            <button style={{position:'relative',right:"44px",margin:"4px",backgroundColor:"transparent",border:'none',color:"blue"}} onClick={Forgot} >Forgot password</button>   
+          </div>
+          
+          </div>}
+            <div style={{display:'flex',justifyContent:'end',position:"relative",zIndex:"10"}}>
+             
+            </div>
+        <div className="relative" style={{display:'flex',justifyContent:'end',width:'200px',top:'-74px',left:"5px"}}>
+           
+        </div>
+        
+        </div>
+      </div>
+      {(create || user) && <div className="box" ></div>}
+      {create && <div className="box"></div>}
+      
+        {!user && create && <div className="timeout center" style={{paddingBottom:"15px",border:'2px solid black',width:"280px",backgroundColor:"beige",position:"relative",top:"-50px"}} >
+        <button className="relative" style={{top:'-70px',left:'205px',background:'none',border:'none',color:"black",fontSize:"50px",zIndex:"10"}} onClick={()=>{setCreate(false)}} >X</button> 
+        <div className="relative" ><br></br><br></br>
+          <form className="center column relative" style={{right:"28px"}} onSubmit={(e)=> CreateUser(e)}>
+            <div style={{padding:'10px',fontSize:"40px"}} >Sign Up</div>
+            <input style={{borderColor: red, width:'180px'}} name='email' type='email' placeholder="email" ></input><br></br>
+            <div className="center" style={{backgroundColor:"transparent",border:'none'}} >
+              <input style={{borderColor: red, width:'180px'}} name='password' type={password} placeholder="password" ></input>
+          </div><br></br>
+              <button className="relative" style={{marginBottom:'10px',right:'69px'}} >Create</button>
+          </form>
+          <div className="relative" style={{display:'flex',justifyContent:'end',top:'-50px',right:'28px'}} >
+            <button onClick={Password}><div className="relative">{password === 'password' ? 'show password' : 'hide password'}</div></button>
+          </div>
+        </div>
+        </div>}
+      <div className="box" >
+      </div>
+      {!user && <div className="box" ></div>}
+      {user && <div className="box center" style={{width:'340px'}} >
+        
+        <button className="topic column" onClick={()=>{router.push('/stats')}} style={{width:'150px', height:"90px"}} >
+          <div className="center" style={{paddingTop:'20px',paddingBottom:"5px"}} >
+            <div className="red relative" style={{marginBottom:"10px",rotate:'90deg',width:'20px',height:'10px',left:'20px',top:"4px"}} ></div>
+            <div className="relative" style={{marginBottom:"10px",rotate:'90deg',width:'40px',height:'10px',backgroundColor:"cyan",top:"-5px",left:"5px"}} ></div>
+            <div className="relative" style={{marginBottom:"10px",rotate:'90deg',width:'30px',height:'10px',left:"-15px",backgroundColor:"lime"}} ></div>
+          </div>
+          Stats
+        </button>
+        <button className="topic column" onClick={()=>{router.push('/stats')}} style={{width: '150px', height:"90px"}} >
+          <div className="center" style={{padding:'0 20px'}}>
+            <div className="relative" style={{rotate:"90deg",borderBottom:"50px solid white",borderRight:'25px solid transparent',borderLeft:'25px solid transparent'}} ></div>
+          </div>
+          Start
+        </button>
+      </div>}
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,165 +1,96 @@
-
-import { useEffect, useState } from "react"
-import { useRouter } from "next/router"
-import { auth } from "@/firebase"
-import { db } from "@/firebase"
-import { addDoc, collection } from "firebase/firestore"
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth"
-
-export default function Home() {
-  const [sign, setSign] = useState()
-  const [create, setCreate] = useState(false)
-  const [user, setUser] = useState()
-  const [red, setRed] = useState()
-  const [ loaded, setLoaded] = useState(false)
-  const [password, setPassword] = useState('password')
-  const [move, setMove] = useState('165px')
-  const [id, setId] = useState('id')
-  const [link, setLink] = useState(false)
-  const [account, setAccount] = useState({
-    title:"", 
-    password:""
-    })
-
-  const router = useRouter()
+import { useState ,useEffect, use } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebase";
 
 
+export default function Math(){
+    const [count, setCount] = useState(0)
+    const [loaded, setLoaded] = useState(true)
+    const router = useRouter() 
+    const {id} = router.query 
 
-  function signIn(){
-    const email = account.title
-    const password = account.password
-    signInWithEmailAndPassword(auth,email,password).then((result)=>{
-      setUser(true)
-      router.push('/login')
-    }).catch(err =>{
-      alert(err)
-    })
-  }
-  
-  function Forgot(){
-    const email = account.title
-    if(email){
-      sendPasswordResetEmail(auth, email).then(()=>{alert('Check your email')})
-    }else{
-      alert('Put in your email') 
-    }
-  }
+    useEffect(()=>{
+        const ID = window.localStorage.getItem('ID')
+        if(!(ID === id)){
+            router.push("/")
+        }
+    },[])
 
-  function CreateUser(e){
-    e.preventDefault()
-    const email = e.target.email.value
-    const password = e.target.password.value
+    return (
+        <div className="beige menu" style={{height: '300vh',backgroundColor:'beige'}}>
+            <h1 style={{marginLeft:'35px',paddingLeft:'15px',margin:"20px",paddingTop:'15px', borderBottom: '2px solid black',width: '100px'}} >Math</h1>
+            
+            {loaded && <button onClick={() => {setCount(1)}} className="topic">Addition</button >}
+            { count === 1 && <Link href= {`/MIT/singleAdd`}><button className="sub-topic zoom">Single digit Addition</button></Link>}
+            { count === 1 && <Link href= {`/MIT/doubleAdd`}><button className="sub-topic zoom">Double digit Addition</button></Link>}
+            
+            { loaded && <button onClick={() => {setCount(2)}}  className="topic">Subtraction</button >}
+            {  count === 2 &&<Link href= {`/MIT/singleMinus`}><button className="sub-topic zoom">Single digit Subtraction</button></Link>}
+            {  count === 2 && <Link href= {`/MIT/doubleMinus`}><button className="sub-topic zoom">Double digit subtraction</button></Link>}
 
-    createUserWithEmailAndPassword(auth,email,password).then(()=>{
-      setUser(true)
-      router.push('/login')
-    }).catch(err =>{
-      alert(err)
-    })
-}
+            { loaded && <button onClick={() => {setCount(3)}}  className="topic">Multiplication</button >}
+            {  count === 3 &&<Link href= {`/MIT/singleTimes`}><button className="sub-topic zoom">Single digit Multiplication</button></Link>}
+            {   count === 3 && <Link href= {`/MIT/doubleTimes`}><button className="sub-topic zoom">Double digit Multiplication</button></Link>}
 
-  function Password(){
-    if(password === 'password'){
-      setPassword('')
-      setMove('169px')
-    }else{
-      setPassword('password')
-      setMove('165px')
-    }
-  }
+            { loaded && <button onClick={() => {setCount(4)}}  className="topic">Division</button >}
+            {  count === 4 &&<Link href= {`/MIT/longDivision`}><button className="sub-topic zoom">Long Division</button></Link>}
 
-  useEffect(()=>{
-    const SignInUser = window.localStorage.getItem('User')
-    setUser((SignInUser))
-    setLoaded(true)
-    const security = window.localStorage.getItem('Id')
-    if(security){
-      setLink(security)
-    }else{
-      const num = Math.ceil(Math.random()*100000000000000000000)
-      window.localStorage.setItem('Id' , num)
-      setLink(num)
-    }
-  },[])
+            { loaded && <button onClick={() => {setCount(5)}}  className="topic">LCM & HCF</button >}
+            {  count === 5 &&<Link href= {`/MIT/Lcm`}><button className="sub-topic zoom">Lowest Common Multiple</button></Link>}
+            {  count === 5 && <Link href= {`/MIT/Hcf`}><button className="sub-topic zoom">Highest Common Factor</button></Link>}
 
-  useEffect(()=>{
-    if(user === true){
-    window.localStorage.setItem('User', `${auth.currentUser?.email}`)
-    window.localStorage.setItem('uid', `${auth.currentUser?.uid}`)
-    router.push('/login')
-}},[user])
+            { loaded && <button onClick={() => {setCount('fraction')}}  className="topic">Fractions</button >}
+            {  count === 'fraction' &&<Link href= {`/MIT/Simplify`}><button className="sub-topic zoom">Simplify</button></Link>}
+            {  count === 'fraction' &&<Link href= {`/MIT/Mixed`}><button className="sub-topic zoom">Mixed to Improper</button></Link>}
+            {  count === 'fraction' && <Link href= {`/MIT/Improper`}><button className="sub-topic zoom">Improper to Mixed</button></Link>}            
+            {  count === 'fraction' && <Link href= {`/MIT/ACD`}><button className="sub-topic zoom">Addition (common denominator)</button></Link>}            
+            {  count === 'fraction' && <Link href= {`/MIT/AUD`}><button className="sub-topic zoom">Addition (uncommon denominator)</button></Link>}            
+            {  count === 'fraction' && <Link href= {`/MIT/AMF`}><button className="sub-topic zoom">Addition (Mixed Fractions)</button></Link>}            
+            {  count === 'fraction' && <Link href= {`/MIT/SCD`}><button className="sub-topic zoom">Subtraction (common denominator)</button></Link>}            
+            {  count === 'fraction' && <Link href= {`/MIT/SMF`}><button className="sub-topic zoom">Subtraction (Mixed Fractions)</button></Link>}            
+            {  count === 'fraction' && <Link href= {`/MIT/MPF`}><button className="sub-topic zoom">Multiplication (Proper Fractions)</button></Link>}            
+            {  count === 'fraction' && <Link href= {`/MIT/MMF`}><button className="sub-topic zoom">Multiplication (Mixed Fractions)</button></Link>}
+            {  count === 'fraction' && <Link href= {`/MIT/DPF`}><button className="sub-topic zoom">Division (Proper Fractions)</button></Link>}            
+            {  count === 'fraction' && <Link href= {`/MIT/DMF`}><button className="sub-topic zoom">Division (Mixed Fractions)</button></Link>}                        
 
-  return (
-    <div className="center beige column">
-      <div className="center column" style={{alignItems:"start"}}>
-        <div className="relative" style={{fontSize:"70px",top:"20px",color:'navy',padding:"0px"}} >Math</div>
-        <div className="relative" style={{fontSize:"70px",color:'purple',paddingBottom:"20px"}} >Improve</div>
-        <div className="relative">
-          {!user && !create && loaded && <div style={{ display:"flex",flexDirection:"column",alignItems:"center", height:"215px",border:"2px solid black",padding:"5px"}}>
-            <div style={{paddingBottom:'10px',fontSize:"50px"}} >
-            <button style={{background:'none',fontSize:"20px",border:'none',color:"blue",display:"flex",justifyContent:"end",width:"110%",paddingTop:"5px"}} onClick={()=>{setCreate(true)}} >Sign up</button>
-              <div className="center" style={{fontSize:"50px",fontWeight:"bold"}}  >Sign In</div> 
-            </div>  
-             
-            <div className="" style={{width:"200px",backgroundColor:"transparent",border:'none'}} >
-              <input className="relative" style={{borderColor: red,width:'190px',marginBottom:"10px"}} name='email' type='email' placeholder="Email" value={account.title} onChange = {(e) => setAccount({...account, title: e.target.value})} ></input>
-              <input style={{borderColor: red,width:'190px',zIndex:"100"}} name='password' type={password} placeholder="password" value={account.password} onChange = {(e) => setAccount({...account, password: e.target.value})} ></input> </div>
-              {!user && !create && loaded && <button style={{position:"relative",left:"43px"}} onClick={Password}>
-              <div className="relative">{password === 'password' ? 'show password' : 'hide password'}</div></button>}
-          <div className="center column" >
-          <button style={{position:'relative',right:"70px"}} onClick={signIn} >Enter</button>
-            <button style={{position:'relative',right:"44px",margin:"4px",backgroundColor:"transparent",border:'none',color:"blue"}} onClick={Forgot} >Forgot password</button>   
-          </div>
-          
-          </div>}
-            <div style={{display:'flex',justifyContent:'end',position:"relative",zIndex:"10"}}>
-             
-            </div>
-        <div className="relative" style={{display:'flex',justifyContent:'end',width:'200px',top:'-74px',left:"5px"}}>
-           
-        </div>
-        
-        </div>
-      </div>
-      {(create || user) && <div className="box" ></div>}
-      {create && <div className="box"></div>}
-      
-        {!user && create && <div className="timeout center" style={{paddingBottom:"15px",border:'2px solid black',width:"280px",backgroundColor:"beige",position:"relative",top:"-50px"}} >
-        <button className="relative" style={{top:'-70px',left:'205px',background:'none',border:'none',color:"black",fontSize:"50px",zIndex:"10"}} onClick={()=>{setCreate(false)}} >X</button> 
-        <div className="relative" ><br></br><br></br>
-          <form className="center column relative" style={{right:"28px"}} onSubmit={(e)=> CreateUser(e)}>
-            <div style={{padding:'10px',fontSize:"40px"}} >Sign Up</div>
-            <input style={{borderColor: red, width:'180px'}} name='email' type='email' placeholder="email" ></input><br></br>
-            <div className="center" style={{backgroundColor:"transparent",border:'none'}} >
-              <input style={{borderColor: red, width:'180px'}} name='password' type={password} placeholder="password" ></input>
-          </div><br></br>
-              <button className="relative" style={{marginBottom:'10px',right:'69px'}} >Create</button>
-          </form>
-          <div className="relative" style={{display:'flex',justifyContent:'end',top:'-50px',right:'28px'}} >
-            <button onClick={Password}><div className="relative">{password === 'password' ? 'show password' : 'hide password'}</div></button>
-          </div>
-        </div>
-        </div>}
-      <div className="box" >
-      </div>
-      {!user && <div className="box" ></div>}
-      {user && <div className="box center" style={{width:'340px'}} >
-        
-        <button className="topic column" onClick={()=>{router.push('/stats')}} style={{width:'150px', height:"90px"}} >
-          <div className="center" style={{paddingTop:'20px',paddingBottom:"5px"}} >
-            <div className="red relative" style={{marginBottom:"10px",rotate:'90deg',width:'20px',height:'10px',left:'20px',top:"4px"}} ></div>
-            <div className="relative" style={{marginBottom:"10px",rotate:'90deg',width:'40px',height:'10px',backgroundColor:"cyan",top:"-5px",left:"5px"}} ></div>
-            <div className="relative" style={{marginBottom:"10px",rotate:'90deg',width:'30px',height:'10px',left:"-15px",backgroundColor:"lime"}} ></div>
-          </div>
-          Stats
-        </button>
-        <button className="topic column" onClick={()=>{router.push('/stats')}} style={{width: '150px', height:"90px"}} >
-          <div className="center" style={{padding:'0 20px'}}>
-            <div className="relative" style={{rotate:"90deg",borderBottom:"50px solid white",borderRight:'25px solid transparent',borderLeft:'25px solid transparent'}} ></div>
-          </div>
-          Start
-        </button>
-      </div>}
-    </div>
-  )
-}
+            { loaded && <button onClick={() => {setCount('in')}} className="topic" >Inequality</button>}
+            { count === 'in' && <Link href= {`/MIT/Gl`}><button className="sub-topic zoom">Greater than less than</button></Link>}
+
+            { loaded && <button onClick={() => {setCount(6)}} className="topic" >Mean Median & Mode</button>}
+            { count === 6 && <Link href= {`/MIT/mean`}><button className="sub-topic zoom">Mean</button></Link>}
+            { count === 6 && <Link href= {`/MIT/median`}><button className="sub-topic zoom ">Median</button></Link>}
+            { count === 6 && <Link href= {`/MIT/mode`}><button className="sub-topic zoom" >Mode</button></Link>}
+
+            { loaded && <button onClick={() => {setCount(7)}}  className="topic">Exponents/Power</button >}
+            {  count === 7 &&<Link href= {`/MIT/square`}><button className="sub-topic zoom">Square</button></Link>}
+            {  count === 7 && <Link href= {`/MIT/cube`}><button className="sub-topic zoom">Cube</button></Link>}
+
+            { loaded &&  <button onClick={() => {setCount(8)}}  className="topic">Roots</button >}
+            {  count === 8 &&<Link href= {`/MIT/squareRoots`}><button className="sub-topic zoom">Square roots</button></Link>}
+            {  count === 8 && <Link href= {`/MIT/cubeRoots`}><button className="sub-topic zoom">Cube roots</button></Link>}
+
+            { loaded && <button onClick={() => {setCount(9)}}  className="topic">Bedmas</button >}
+            { count === 9 &&<Link href= {`/MIT/Bedmas`}><button className="sub-topic zoom">Bedmas</button></Link>}
+            
+            { loaded && <button onClick={() => {setCount(10)}}  className="topic">Algebra</button >}
+            { count === 10 && <Link href= {`/MIT/Algebra`}><button className="sub-topic zoom">One variable</button></Link>}
+            { count === 10 && <Link href= {`/MIT/likeTerm`}><button className="sub-topic zoom">Like terms</button></Link>}
+            { count === 10 && <Link href= {`/MIT/Algebra2`}><button className="sub-topic zoom">Two variable</button></Link>}
+            { count === 10 && <Link href= {`/MIT/Foil`}><button className="sub-topic zoom">Foil</button></Link>}
+            { count === 10 && <Link href= {`/MIT/Trinomial`}><button className="sub-topic zoom">Factor trinomial</button></Link>}
+            { count === 10 && <Link href= {`/MIT/Quadratic`}><button className="sub-topic zoom">Quadratic formula</button></Link>}
+
+            
+            { loaded && <button onClick={() => {setCount('angle')}}  className="topic">Geometry</button >}
+            { count === 'angle' &&<Link href= {`/MIT/TypeAngle`}><button className="sub-topic zoom">Types of angle</button></Link>}
+            { count === 'angle' &&<Link href= {`/MIT/Perimeter`}><button className="sub-topic zoom">Perimeter</button></Link>}
+            { count === 'angle' &&<Link href= {`/MIT/Circumference`}><button className="sub-topic zoom">Circumference</button></Link>}
+            { count === 'angle' &&<Link href= {`/MIT/Area`}><button className="sub-topic zoom">Area</button></Link>}
+
+            { loaded && <button onClick={() => {setCount('log')}}  className="topic">Logarithms</button >}
+            { count === 'log' &&<Link href= {`/MIT/Logarithm`}><button className="sub-topic zoom">Logarithms</button></Link>}
+        </div>     
+    )
+} 
