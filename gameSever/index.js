2860b6b882c75c433dd421f21921809a10709ea4
Kamal <kamal.raghe33@gmail.com>
2024-10-29 15:13:34 -0400

    commit

--- gameSever/index.js
+++ gameSever/index.js
@@ -8,25 +8,31 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "https://mathimproved.com/Game",
-    methods: ["GET", "POST"],
-  },
-});
 
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-  });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
-});
 
-server.listen(3001, () => {
-  console.log("SERVER IS RUNNING");
-});
+// WebSocket
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "https://mathimproved.com/Game",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//   });
+
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+// });
+
+// server.listen(3001, () => {
+//   console.log("SERVER IS RUNNING");
+// });
