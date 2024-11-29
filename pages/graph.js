611ec66d985fe529f32bebc6f1056301a145d72e
Kamal <kamal.raghe33@gmail.com>
2024-11-28 20:52:09 -0500

    commit

--- pages/graph.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import Plot from "react-plotly.js";
-import { evaluate, re } from "mathjs"; // For parsing mathematical expressions
-
-export default function FourQuadrantGraph(){
- return(
-    <div>hello</div>
- )
-};
