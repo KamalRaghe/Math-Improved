4196cab060eff0226640ba222f6e3267fe72fb67
Kamal <kamal.raghe33@gmail.com>
2024-12-05 14:24:09 -0500

    commit

--- pages/graph.js
+++ pages/graph.js
@@ -72,7 +72,7 @@ const FourQuadrantGraph = () => {
           showgrid: true,
           gridcolor: "#ddd",
           range: [-range, range],
-          dtick: range / 5, // Synchronize x-axis tick spacing
+          dtick: range / 10, // Synchronize x-axis tick spacing
           tickangle: 0, // Rotate tick labels 45 degrees counterclockwise
         },
         yaxis: {
@@ -83,7 +83,7 @@ const FourQuadrantGraph = () => {
           gridcolor: "#ddd",
           scaleanchor: "x", // Ensures equal scaling
           range: [-range, range],
-          dtick: range / 5, // Synchronize y-axis tick spacing
+          dtick: range / 10, // Synchronize y-axis tick spacing
         },
         shapes: [
           {
@@ -104,7 +104,7 @@ const FourQuadrantGraph = () => {
           },
         ],
       }}
-      style={{ width: "100vh", height: "100vh" }}
+      style={{ width: "100vh", height: "100vh", padding:"20px" }}
     />
     </div>
   );
