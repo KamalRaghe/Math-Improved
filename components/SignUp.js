5c9fa621481d14bf1432584196bbaf5f3d441d6a
Kamal <kamal.raghe33@gmail.com>
2024-12-07 16:48:02 -0500

    commit

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,13 +1,17 @@
+import { useRouter } from "next/router"
 
 export default function Sign(){
+    const router = useRouter()
     return(
         <div className="center zoom" style={{backgroundColor:"beige",zIndex:"200",width:"100%",height:"100%",position:"absolute"}} >
             <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown',backgroundColor:"beige"}}>
-                <div> You reach the limit for this topic</div>
-                <div>Try a new topic </div>
+                <div className="font" > You reach the limit for this topic</div>
+                <br></br>
+                <div className="font">Try a new topic </div>
+                <br></br>
                 <div>or</div>
                 <div><button className="sub-topic" onClick={()=>{router.push('/Sign')}} >Sign up</button></div>
-                <div style={{fontSize:"20px"}} >7 day free trial </div>
+                <div className="font" > 7 day free trial </div>
             </div>
         </div>
     )
