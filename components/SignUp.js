1df6c865884ccbf83704a2668dcc596c3d451720
Kamal <kamal.raghe33@gmail.com>
2024-12-07 16:43:33 -0500

    commit

--- components/SignUp.js
+++ components/SignUp.js
@@ -2,8 +2,12 @@
 export default function Sign(){
     return(
         <div className="center zoom" style={{backgroundColor:"beige",zIndex:"200",width:"100%",height:"100%",position:"absolute"}} >
-            <div className="Help column" style={{zIndex:'30', border:'1px solid brown',backgroundColor:"beige"}}>
-                <button className="sub-topic" onClick={()=>{router.push('/Sign');console.log('RG')}} >Sign up</button>
+            <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown',backgroundColor:"beige"}}>
+                <div> You reach the limit for this topic</div>
+                <div>Try a new topic </div>
+                <div>or</div>
+                <div><button className="sub-topic" onClick={()=>{router.push('/Sign')}} >Sign up</button></div>
+                <div style={{fontSize:"20px"}} >7 day free trial </div>
             </div>
         </div>
     )
