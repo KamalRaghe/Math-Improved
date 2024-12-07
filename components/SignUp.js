bc8d29ad873357d607d2c29a7c31e570ba40547f
Kamal <kamal.raghe33@gmail.com>
2024-12-07 15:58:51 -0500

    commit

---
+++ components/SignUp.js
@@ -0,0 +1,12 @@
+
+export default function HelpAdd(){
+    return(
+        <div style={{backgroundColor:"beige",zIndex:"200",width:"100%",height:"100%"}} >
+            <div className="Help column" style={{zIndex:'30', border:'10px solid yellow',backgroundColor:"beige"}}>
+
+            </div>
+        </div>
+    )
+    
+   
+}
