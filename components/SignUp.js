d60994cd2e2da12f4927e5d848e1d1f5ffd83ac3
Kamal <kamal.raghe33@gmail.com>
2024-12-07 16:14:24 -0500

    commit

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,9 +1,9 @@
 
 export default function Sign(){
     return(
-        <div className="center" style={{backgroundColor:"beige",zIndex:"200",width:"100%",height:"100%",position:"absolute"}} >
+        <div className="center zoom" style={{backgroundColor:"beige",zIndex:"200",width:"100%",height:"100%",position:"absolute"}} >
             <div className="Help column" style={{zIndex:'30', border:'1px solid brown',backgroundColor:"beige"}}>
-
+                <button className="sub-topic" onClick={()=>{router.push('/Sign');console.log('RG')}} >Sign up</button>
             </div>
         </div>
     )
