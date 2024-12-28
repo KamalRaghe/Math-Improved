b145533b0d48a371c13f2499a03f43904cdf5e3b
Kamal <kamal.raghe33@gmail.com>
2024-12-28 14:54:36 -0500

    commit

--- components/feedback.js
+++ components/feedback.js
@@ -19,6 +19,9 @@ export default function FeedBack(){
     return(
         <div className="center zoom" style={{backgroundColor:"beige",zIndex:"200",width:"100%",height:"100%",position:"absolute"}} >
             <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown',backgroundColor:"beige"}}>
+                 <div className='cancel' style={{width:"100%"}} >
+                    <button className='cancel-btn' style={{fontSize:"25px",margin:"0px",position:"relative",bottom:"8px"}}>X</button>
+                </div>
                  <textarea placeholder="Feedback"></textarea>
                  <br></br>
                  <button onClick={()=>router.reload()}>Submit</button>
--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,10 @@ export default function Math(){
     return (
         <div className="beige menu" style={{height: '300vh',backgroundColor:'beige'}}>
             {check && <FeedBack/>}
-            <div className="center" style={{position:"relative",justifyContent:"end",width:"100vw",zIndex:"100"}} >
+            <div className="center" style={{position:"relative",justifyContent:"end",width:"97vw",zIndex:"100",marginRight:"400px"}} >
+                <button className="sub-topic" onClick={()=>{setCheck(true)}} >Feedback</button>
                 <button className="sub-topic" onClick={()=>{router.push('/Sign')}} >Sign in</button>
+            </div>
             <div style={{position:"relative",bottom:"100px",zIndex:"1"}} >
                 <h1 style={{marginLeft:'35px',paddingLeft:'15px',paddingRight:'15px',margin:"20px",paddingTop:'15px', borderBottom: '2px solid black',width: '90px'}} >Math</h1>
                 
