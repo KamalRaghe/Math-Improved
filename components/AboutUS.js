7503c283e5c3ba78676da20c8bb63217a7fd56f4
Kamal <kamal.raghe33@gmail.com>
2024-12-31 15:00:50 -0500

    commit

--- components/AboutUS.js
+++ components/AboutUS.js
@@ -11,13 +11,15 @@ export default function AboutUs(){
     })
     return(
         <div className="center zoom" style={{backgroundColor:"beige",zIndex:"200",width:"100%",height:"100%",position:"absolute"}} >
-            <button className='cancel-btn' style={{fontSize:"25px",margin:"0px",position:"relative",bottom:"8px"}}  onClick = {close}>X</button>
             <div className=" column center" style={{borderRadius:"20px",padding:"20px",width:"300px",border:'2px solid brown',backgroundColor:"beige"}}>
-            <h1>Mission</h1>
-            To create a supportive and engaging learning environment where students can build strong foundations in mathematics, empowering them to overcome challenges, explore their full potential, and develop a lifelong love for learning.
+            <button className='cancel-btn' style={{fontSize:"25px",margin:"0px",position:"relative",left:"140px",alignItems:"end"}}  onClick = {close}>X</button>
+            <div className="center column" style={{position:"relative",bottom:"20px"}}>
+                <h2>Mission</h2>
+                To create a supportive and engaging learning environment where students can build strong foundations in mathematics, empowering them to overcome challenges, explore their full potential, and develop a lifelong love for learning no matter their grade level.
 
-            <h1>Vision</h1>
-            To become the go-to platform for students to truly understand and excel in mathematics, fostering curiosity, confidence, and mastery in learners of all levels.
+                <h2>Vision</h2>
+                To become the go-to platform for students to truly understand and excel in mathematics, fostering curiosity, confidence, and mastery in learners of all levels.
+            </div>
 
             </div>
         </div>
