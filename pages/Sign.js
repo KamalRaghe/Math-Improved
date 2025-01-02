ad58df48bae9004175066627f30653fe60a8b68d
Kamal <kamal.raghe33@gmail.com>
2025-01-02 01:05:31 -0500

    commit

--- pages/Sign.js
+++ pages/Sign.js
@@ -120,7 +120,7 @@ export default function Home() {
       {create && <div className="box"></div>}
       
         {!user && create && <div className="center" style={{width:"280px",backgroundColor:"beige",position:"relative",top:"-50px"}} >
-        <div className="relative center" style={{border:"2px solid black",width:"220px",paddingLeft:"30px"}} >
+        <div className="relative center" style={{border:"2px solid black",width:"220px"}} >
         <button className="relative" style={{top:'-60px',left:'235px',background:'none',border:'none',color:"black",fontSize:"50px",zIndex:"10"}} onClick={()=>{setCreate(false)}} >X</button> 
           <form className="center column relative" onSubmit={(e)=> CreateUser(e)}>
             <div className="center" style={{padding:'10px',fontSize:"40px"}} >Sign Up</div>
