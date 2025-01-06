d0549fe3af96b66a84e9103ca9973ca774146be6
Kamal <kamal.raghe33@gmail.com>
2025-01-06 15:08:23 -0500

    commit

--- components/wrong.js
+++ components/wrong.js
@@ -1,7 +1,8 @@
 function Wrong(){
     return(
-        <div className='center beige absolute'style={{zIndex:"100"}} >
-            <img className="check" style={{borderRadius:"50%",width:'500px',height:"500px",position:"absolute",backgroundColor:"beige"}} src={'/Red.png'} alt="Wrong" />
+        <div className='center column beige absolute'style={{zIndex:"100",width:"100vw"}}>
+            <h1 className="Red center" style={{scale:"1.8"}} >Try again</h1>
+            <img className="check" style={{borderRadius:"50%",width:'500px',height:"500px",position:"relative",bottom:"20px"}} src={'/Red.png'} alt="Wrong" />
         </div>  
     )    
 }
