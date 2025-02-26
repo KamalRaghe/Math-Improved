09b6670b9bdfda2d1e41df1d856fb42d7169b7c4
Kamal <kamal.raghe33@gmail.com>
2025-02-26 14:46:16 -0500

    commit

--- pages/graph.js
+++ pages/graph.js
@@ -7,19 +7,43 @@ import Correct from '@/components/correct';
 const Plotly = dynamic(() => import('react-plotly.js'), { ssr: false });
 
 const PlotlyGraph = ({x,y,mix}) => {
-    const [xAxis,setXAxis] = useState([4,-1,2,1,-2,.5,-3,-4,-5,3])
-    const [yAxis,setYAxis] = useState([-1,5,-2,-3,1,-4,5,-5,3,4])
+    const [xAxis,setXAxis] = useState([4,-1,2,1,-2,.5,-3,-4,-5])
+    const [yAxis,setYAxis] = useState([-1,5,-2,-3,1,-4,5,-5,3])
     const [correct, setCorrect] = useState(false)
     const[ wrong, setWrong] = useState(false)
     const data = [
         {
-            x: [xAxis[1],xAxis[3],xAxis[5],xAxis[7],x], // Quadrant I
+            x: [xAxis[1],xAxis[3],xAxis[5],xAxis[7],x],
             y: [yAxis[1],yAxis[3],yAxis[5],yAxis[7],y],
             mode: 'markers',
-            hoverinfo: 'none', // Disable hover info
+            hoverinfo: 'none', 
             type: 'scatter',
             marker: { color: 'navy', size: 10 },
         },
+        {
+            x: [3,3,3,3],
+            y: [0,1,2,3],
+            mode: 'markers',
+            hoverinfo: 'none', 
+            type: 'scatter',
+            marker: { color: 'green', size: 10, opacity: 0.6 },
+        },
+        {
+            x: [3],
+            y: [4],
+            mode: 'markers',
+            hoverinfo: 'none', 
+            type: 'scatter',
+            marker: { color: 'green', size: 10 },
+        },
+        {
+            x: [1,2,3],
+            y: [0,0,0],
+            mode: 'markers',
+            hoverinfo: 'none', 
+            type: 'scatter',
+            marker: { color: 'gold', size: 10, opacity: 0.8 },
+        },  
     ];
 
     const layout = {
@@ -82,14 +106,14 @@ const PlotlyGraph = ({x,y,mix}) => {
     },[])
 
     return (
-        <div className='center' >
+        <div className='center' style={{cursor:"pointer"}} >
             {wrong && <Wrong></Wrong>}
             {correct && <Correct></Correct>}
             <Plotly
             data={data}
             layout={layout}
             onClick={handleClick} // Handle point click
-            style={{ width: '400px', height: '400px' }}
+            style={{ width: '400px', height: '400px'}}
             useResizeHandler={true} // Make the graph responsive
         />
         </div>
