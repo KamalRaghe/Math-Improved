40a84380d9b3f6a360cafcc2c8dcb113c9935bdb
Kamal <kamal.raghe33@gmail.com>
2025-02-26 19:17:59 -0500

    commit

--- pages/slope.js
+++ pages/slope.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Choice from "@/components/choice";
+import Choice from "@/components/FractionChoice";
 import Correct from "@/components/correct";
 import Wrong from "@/components/wrong"; 
 import HelpCube from "@/components/cubehelp";
@@ -13,10 +13,10 @@ export default function DoubleAdd(){
     const [loaded, setLoaded] = useState(false)
     const [correct, setCorrect] = useState(false)
     const[ wrong, setWrong] = useState(false)
-    const [num1, setNum1] = useState(Math.ceil(Math.random()*9));
-    const [num2, setNum2] = useState(Math.ceil(Math.random()*9));
-    const [num4, setNum4] = useState(Math.ceil(Math.random()*9));
-    const [num5, setNum5] = useState(Math.ceil(Math.random()*9));
+    const [num1, setNum1] = useState(Math.ceil(Math.random()*5));
+    const [num2, setNum2] = useState(Math.ceil(Math.random()*5));
+    const [num4, setNum4] = useState(Math.ceil(Math.random()*5+5));
+    const [num5, setNum5] = useState(Math.ceil(Math.random()*5+5));
     const [num3, setNum3] = useState([0,num1+num1,num1+num1+num1,-1*num1,num1])
     const router = useRouter()
     const {username} = router.query 
@@ -111,17 +111,17 @@ export default function DoubleAdd(){
             {help && <HelpCube  num1={num1} close={close}/>}
             {loaded && correct && <Correct></Correct>}
             {loaded && wrong && <Wrong/> }
-            {count > 10 && score < 100  && <Sign></Sign>}
-            {score > 100  && <Sign2></Sign2>}
+            {/* {count > 10 && score < 100  && <Sign></Sign>}
+            {score > 100  && <Sign2></Sign2>} */}
             <div className="box column">
-               <div className="row ">
-                    { loaded && <Choice value ={((num2-num5)/(num1-num4))+num3[0]} answer ={((num2-num5)/(num1-num4))} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && <Choice value ={((num2-num5)/(num1-num4))+num3[1]} answer ={((num2-num5)/(num1-num4))} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && <Choice value ={((num2-num5)/(num1-num4))+num3[2]} answer ={((num2-num5)/(num1-num4))} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+            <div className="row ">
+                    { loaded && <Choice whole={num4-num1 >= num5-num2 && Math.floor((num4-num1)/(num5-num2))} value1 ={((num5-num2)%(num4-num1)+num3[0]) } answer1 ={(num5-num2)%(num4-num1)} value2={num4-num1} answer2={num4-num1} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice whole={num4-num1 >= num5-num2 && Math.floor((num4-num1)/(num5-num2))} value1 ={((num5-num2)%(num4-num1)+num3[1]) } answer1 ={(num5-num2)%(num4-num1)} value2={num4-num1} answer2={num4-num1} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice whole={num4-num1 >= num5-num2 && Math.floor((num4-num1)/(num5-num2))} value1 ={((num5-num2)%(num4-num1)+num3[2]) } answer1 ={(num5-num2)%(num4-num1)} value2={num4-num1} answer2={num4-num1} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
                </div>
                <div className="row">
-                    { loaded && <Choice value ={((num2-num5)/(num1-num4))+num3[3]} answer ={((num2-num5)/(num1-num4))} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
-                    { loaded && <Choice value ={((num2-num5)/(num1-num4))+num3[4]} answer ={((num2-num5)/(num1-num4))} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice whole={num4-num1 >= num5-num2 && Math.floor((num4-num1)/(num5-num2))} value1 ={((num5-num2)%(num4-num1)+num3[3]) } answer1 ={(num5-num2)%(num4-num1)} value2={num4-num1} answer2={num4-num1} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
+                    { loaded && <Choice whole={num4-num1 >= num5-num2 && Math.floor((num4-num1)/(num5-num2))} value1 ={((num5-num2)%(num4-num1)+num3[4]) } answer1 ={(num5-num2)%(num4-num1)} value2={num4-num1} answer2={num4-num1} doSomething = {Add} Correct={CorrectA} Wrong={WrongA}/>}
                </div>
             </div>
         </div>
