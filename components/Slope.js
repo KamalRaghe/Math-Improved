ffdf3201ec7161096e49e03d9d95ec876be71a65
Kamal <kamal.raghe33@gmail.com>
2025-03-13 07:05:15 -0400

    commit

--- components/Slope.js
+++ components/Slope.js
@@ -94,10 +94,10 @@ function Slope({close, num1 , num2 , num4 , num5}){
                    {<button className="choice red" onClick={close} >Close</button>}                   
                </div>}
                {dextraadd && <div className='center wrap absolute StepAnswer' style={{width:'100%',top:"340px"}} >
-                   <StepF whole={Math.floor(((num1+(whole1*num2))*((num4+(whole2*num5))))/(num5*num2))} value1 = {(((num1+(whole1*num2))*((num4+(whole2*num5))))%(num5*num2))+arr[0]}  answer1={((num1+(whole1*num2))*((num4+(whole2*num5))))%(num5*num2)} value2 = {(num5*num2)}  answer2={num5*num2}  Count ={Count} mistake={Nothing}/>
-                   <StepF whole={Math.floor(((num1+(whole1*num2))*((num4+(whole2*num5))))/(num5*num2))} value1 = {(((num1+(whole1*num2))*((num4+(whole2*num5))))%(num5*num2))+arr[2]}  answer1={((num1+(whole1*num2))*((num4+(whole2*num5))))%(num5*num2)} value2 = {(num5*num2)}  answer2={num5*num2}  Count ={Count} mistake={Nothing}/>
-                   <StepF whole={Math.floor(((num1+(whole1*num2))*((num4+(whole2*num5))))/(num5*num2))} value1 = {(((num1+(whole1*num2))*((num4+(whole2*num5))))%(num5*num2))+arr[3]}  answer1={((num1+(whole1*num2))*((num4+(whole2*num5))))%(num5*num2)} value2 = {(num5*num2)}  answer2={num5*num2}  Count ={Count} mistake={Nothing}/>
-                   <StepF whole={Math.floor(((num1+(whole1*num2))*((num4+(whole2*num5))))/(num5*num2))} value1 = {(((num1+(whole1*num2))*((num4+(whole2*num5))))%(num5*num2))+arr[1]}  answer1={((num1+(whole1*num2))*((num4+(whole2*num5))))%(num5*num2)} value2 = {(num5*num2)}  answer2={num5*num2}  Count ={Count} mistake={Nothing}/>
+                   <StepF whole={Math.floor((num5-num2)/(num4-num1))} value1 = {(num5-num2)+arr[0]}  answer1={(num5-num2)} value2 = {(num5*num2)}  answer2={num5*num2}  Count ={Count} mistake={Nothing}/>
+                   <StepF whole={Math.floor((num5-num2)/(num4-num1))} value1 = {(num5-num2)+arr[2]}  answer1={(num5-num2)} value2 = {(num5*num2)}  answer2={num5*num2}  Count ={Count} mistake={Nothing}/>
+                   <StepF whole={Math.floor((num5-num2)/(num4-num1))} value1 = {(num5-num2)+arr[3]}  answer1={(num5-num2)} value2 = {(num5*num2)}  answer2={num5*num2}  Count ={Count} mistake={Nothing}/>
+                   <StepF whole={Math.floor((num5-num2)/(num4-num1))} value1 = {(num5-num2)+arr[1]}  answer1={(num5-num2)} value2 = {(num5*num2)}  answer2={num5*num2}  Count ={Count} mistake={Nothing}/>
                    {<button className="choice" style={{backgroundColor:'yellow',color:'black'}} onClick={()=>{setExtra(true);console.log(num4+(whole2*num5))*num2}} >help</button>}
                </div>}
            </div>
