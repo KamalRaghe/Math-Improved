dfcee6642b6ed6ffb3f9da9e9008e5b090719f26
Kamal <kamal.raghe33@gmail.com>
2025-03-14 04:35:04 -0400

    commit

--- components/solveB.js
+++ components/solveB.js
@@ -16,7 +16,7 @@ export default function SolveB({num1,num2,slope,close}){
     const [x, setX] = useState('𝑥')
     const [y, setY] = useState('y')
     const [m, setM] = useState('m')
-    const [sign ,setSign] = useState('')
+    const [sign ,setSign] = useState('x')
     const [answer, setAnswer] = useState(num1*4)
     const [arr, setArr]=useState([0,Math.floor(Math.random()*1+2)+1,1,Math.floor(Math.random()*3)-4])
 
@@ -42,7 +42,7 @@ export default function SolveB({num1,num2,slope,close}){
 
     return (
         <div className="Help center column" style={{zIndex:'50'}}>
-            <div className='cancel' style={{width:"100%"}} ><button className='cancel-btn' onClick = {close}>X</button></div>
+            <div className='cancel' style={{width:"100%",bottom:'40px',border:"1px solid black",margin:"0px"}} ><button className='cancel-btn' onClick = {close}>X</button></div>
             {extra && sign === '+' && Q1 < 10 && Q2 < 10 && <HelpAdd close={Extra} num1 ={Q1} num2 = {Q2}/>}
             {extra && sign === '+' && (Q1 >= 10 || Q2 >= 10) && <StepAdd close={Extra} num1 ={Q1} num2 = {Q2}/>}
             {extra && sign === 'x' && Q1 < 10 && Q2 < 10 && <HelpTimes close={Extra} num1 ={Q1} num2 = {Q2}/>}
@@ -52,9 +52,10 @@ export default function SolveB({num1,num2,slope,close}){
             {extra && sign === '÷' && <HelpDiv close={Extra} num1 ={Q1} num2 = {Q2}/>}
                <div> 
                <div className="double center column ">
-                    <div style={{margin:"20px"}} >{y} = {m}{x}  + b</div>
+                    <div style={{margin:"20px",marginTop:"50px"}} >{y} = {m}{x}  + b</div>
                     <div className="Green carry" >{x === '𝑥' && <span onClick={()=>{setX(`(${num1})`)}} >𝑥  = {num1}</span>} {y === 'y' &&<span onClick={()=>{setY(num2)}} >{x === '𝑥' &&<span className="hide" >0</span>}y = {num2}</span> } </div> 
                     { m === 'm' && <div className="Green carry" onClick={()=>{setM(slope)}}>m = {slope} </div>}
+                    <div className="hide" >1</div>
                 </div>    
                </div>
                <div className="box" ></div>
