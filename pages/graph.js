8a0efff91d423411fbd763df9c2cb48063a86c2b
Kamal <kamal.raghe33@gmail.com>
2025-03-31 11:55:48 -0400

    commit

--- pages/graph.js
+++ pages/graph.js
@@ -35,109 +35,105 @@ export default function CurrencyConverter() {
     BYN: "Belarusian Ruble",
     BZD: "Belize Dollar",
     CAD: "Canadian Dollar",
+    CDF: "Congolese Franc",
     CHF: "Swiss Franc",
     CLP: "Chilean Peso",
     CNY: "Chinese Yuan",
     COP: "Colombian Peso",
     CRC: "Costa Rican Colón",
     CUP: "Cuban Peso",
+    CVE: "Cape Verdean Escudo",
     CZK: "Czech Koruna",
+    DJF: "Djiboutian Franc",
     DKK: "Danish Krone",
     DOP: "Dominican Peso",
     DZD: "Algerian Dinar",
     EGP: "Egyptian Pound",
+    ERN: "Eritrean Nakfa",
+    ETB: "Ethiopian Birr",
     EUR: "Euro",
+    FJD: "Fijian Dollar",
+    FKP: "Falkland Islands Pound",
     GBP: "British Pound Sterling",
+    GEL: "Georgian Lari",
+    GHS: "Ghanaian Cedi",
+    GIP: "Gibraltar Pound",
+    GMD: "Gambian Dalasi",
+    GNF: "Guinean Franc",
+    GTQ: "Guatemalan Quetzal",
+    GYD: "Guyanese Dollar",
     HKD: "Hong Kong Dollar",
+    HNL: "Honduran Lempira",
+    HRK: "Croatian Kuna",
+    HTG: "Haitian Gourde",
     HUF: "Hungarian Forint",
     IDR: "Indonesian Rupiah",
     ILS: "Israeli New Shekel",
     INR: "Indian Rupee",
+    IQD: "Iraqi Dinar",
+    IRR: "Iranian Rial",
     ISK: "Icelandic Króna",
+    JMD: "Jamaican Dollar",
+    JOD: "Jordanian Dinar",
     JPY: "Japanese Yen",
+    KES: "Kenyan Shilling",
+    KGS: "Kyrgyzstani Som",
+    KHR: "Cambodian Riel",
+    KMF: "Comorian Franc",
     KRW: "South Korean Won",
     KWD: "Kuwaiti Dinar",
+    KYD: "Cayman Islands Dollar",
     KZT: "Kazakhstani Tenge",
+    LAK: "Lao Kip",
     LBP: "Lebanese Pound",
     LKR: "Sri Lankan Rupee",
+    LYD: "Libyan Dinar",
     MAD: "Moroccan Dirham",
+    MDL: "Moldovan Leu",
+    MGA: "Malagasy Ariary",
+    MKD: "Macedonian Denar",
+    MMK: "Burmese Kyat",
+    MNT: "Mongolian Tögrög",
+    MOP: "Macanese Pataca",
+    MUR: "Mauritian Rupee",
+    MVR: "Maldivian Rufiyaa",
+    MWK: "Malawian Kwacha",
     MXN: "Mexican Peso",
     MYR: "Malaysian Ringgit",
+    NAD: "Namibian Dollar",
     NGN: "Nigerian Naira",
     NOK: "Norwegian Krone",
+    NPR: "Nepalese Rupee",
     NZD: "New Zealand Dollar",
+    OMR: "Omani Rial",
+    PAB: "Panamanian Balboa",
+    PEN: "Peruvian Sol",
+    PGK: "Papua New Guinean Kina",
     PHP: "Philippine Peso",
     PKR: "Pakistani Rupee",
     PLN: "Polish Złoty",
     QAR: "Qatari Riyal",
     RON: "Romanian Leu",
     RUB: "Russian Ruble",
+    RWF: "Rwandan Franc",
     SAR: "Saudi Riyal",
+    SDG: "Sudanese Pound",
     SEK: "Swedish Krona",
     SGD: "Singapore Dollar",
+    SOS: "Somali Shilling",
+    SYP: "Syrian Pound",
     THB: "Thai Baht",
     TRY: "Turkish Lira",
-    TWD: "New Taiwan Dollar",
+    TZS: "Tanzanian Shilling",
     UAH: "Ukrainian Hryvnia",
+    UGX: "Ugandan Shilling",
     USD: "United States Dollar",
     UYU: "Uruguayan Peso",
     VND: "Vietnamese Dong",
-    ZAR: "South African Rand"
+    XAF: "Central African CFA Franc",
+    XCD: "East Caribbean Dollar",
+    XOF: "West African CFA Franc",
+    XPF: "CFP Franc",
+    ZAR: "South African Rand",
+    ZMW: "Zambian Kwacha"
   };
-
-  useEffect(() => {
-    fetch("https://api.exchangerate-api.com/v4/latest/USD")
-      .then((response) => response.json())
-      .then((data) => {
-        setRates(data.rates);
-        setCurrencies(Object.keys(data.rates));
-      });
-  }, []);
-
-  useEffect(() => {
-    if (rates[fromCurrency] && rates[toCurrency]) {
-      const conversion = (amount / rates[fromCurrency]) * rates[toCurrency];
-      setConvertedAmount(conversion.toFixed(2));
-    }
-  }, [amount, fromCurrency, toCurrency, rates]);
-
-  return (
-    <div className="flex flex-col items-center p-6 bg-gray-100 min-h-screen">
-      <h1 className="text-2xl font-bold mb-4">Currency Converter</h1>
-      <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col gap-4">
-        <input
-          type="number"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-          className="p-2 border rounded"
-        />
-        <div className="flex gap-4">
-          <select
-            value={fromCurrency}
-            onChange={(e) => setFromCurrency(e.target.value)}
-            className="p-2 border rounded"
-          >
-            {currencies.map((currency) => (
-              <option key={currency} value={currency}>
-                {currency} - {countryNames[currency]}
-              </option>
-            ))}
-          </select>
-          <span className="self-center">➡️</span>
-          <select
-            value={toCurrency}
-            onChange={(e) => setToCurrency(e.target.value)}
-            className="p-2 border rounded"
-          >
-            {currencies.map((currency) => (
-              <option key={currency} value={currency}>
-                {currency}  -  {countryNames[currency]}
-              </option>
-            ))}
-          </select>
-        </div>
-        <h2 className="text-xl font-semibold">Converted Amount: {convertedAmount} {toCurrency}</h2>
-      </div>
-    </div>
-  );
-}
