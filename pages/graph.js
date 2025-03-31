0f27e3251fcee50146e55c65ba458a58365e28fe
Kamal <kamal.raghe33@gmail.com>
2025-03-31 10:44:05 -0400

    commit

--- pages/graph.js
+++ pages/graph.js
@@ -131,7 +131,7 @@ export default function CurrencyConverter() {
           >
             {currencies.map((currency) => (
               <option key={currency} value={currency}>
-                {countryNames[currency] &&  currency} } - {countryNames[currency]}
+                countryNames[currency] && {currency} - {countryNames[currency]}
               </option>
             ))}
           </select>
