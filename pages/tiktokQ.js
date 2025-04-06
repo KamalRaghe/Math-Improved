45f394d811ebbb3d07b543dcaff067015420ba0c
Kamal <kamal.raghe33@gmail.com>
2025-04-06 19:25:13 -0400

    commit

--- pages/tiktokQ.js
+++ pages/tiktokQ.js
@@ -2,7 +2,7 @@ export default function tikTok(){
     return(
         <div className="double column center" style={{background:"white",height:"100vh"}} >
             <div>Solve in 3s</div>
-            <div> 2x - 4 = 6 </div>
+            <div style={{margin:"100px"}}> 2x - 4 = 6 </div>
         </div>
     )
 }
