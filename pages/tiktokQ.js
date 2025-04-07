bd3482bdd8474a238d62aea56e594409c11e2cb6
Kamal <kamal.raghe33@gmail.com>
2025-04-07 16:39:33 -0400

    commit

--- pages/tiktokQ.js
+++ pages/tiktokQ.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { FaHandPointDown } from "react-icons/fa";
 export default function tikTok(){
     const [count, setCount] = useState(4)
+    setTimeout(() => {
+        setCount(3)
+    }, 1000);
     return(
         <div className="double column center" style={{background:"white",height:"100vh"}} >
             <div>Solve this</div>
