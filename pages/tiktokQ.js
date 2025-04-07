bbc3475d319292e217829c87c38a49191dfdbbba
Kamal <kamal.raghe33@gmail.com>
2025-04-07 13:29:00 -0400

    commit

--- pages/tiktokQ.js
+++ pages/tiktokQ.js
@@ -2,7 +2,7 @@ import { FaHandPointDown } from "react-icons/fa";
 export default function tikTok(){
     return(
         <div className="double column center" style={{background:"white",height:"100vh"}} >
-            <div>Solve in 3s</div>
+            <div>Solve this</div>
             <div style={{margin:"50px"}}> 2x - 4 = 6 </div>
             <div>Answer <span style={{position:"relative",top:'10px'}} ><FaHandPointDown></FaHandPointDown></span></div>
         </div>
