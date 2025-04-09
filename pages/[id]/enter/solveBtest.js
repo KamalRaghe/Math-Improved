6a430dc8f38d3ed5e9a1d9f97a77ee25614e6be3
Kamal <kamal.raghe33@gmail.com>
2025-04-09 10:57:46 -0400

    commit

--- pages/[id]/enter/solveBtest.js
+++ pages/[id]/enter/solveBtest.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Choice from "@/components/FractionChoice";
+import Choice from "@/components/Choice";
 import { useRouter } from "next/router";
 import Heart from "@/components/heart";
 import Heart1 from "@/components/heart1";
