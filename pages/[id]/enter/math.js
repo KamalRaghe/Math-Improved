0f098fdc0f5e9cec74e15cd2722076714ca07ce6
Kamal <kamal.raghe33@gmail.com>
2025-05-06 12:44:40 -0400

    commit

--- pages/[id]/enter/math.js
+++ pages/[id]/enter/math.js
@@ -150,7 +150,7 @@ export default function Math(){
             { count === 'log' &&<Link href= {`/${id}/enter/Logarithm`}><button className="sub-topic zoom">Logarithms</button></Link>}
 
             { loaded && <button onClick={() => {setCount('log')}}  className="topic">Logarithms</button >}
-            { count === 'log' &&<Link href= {`/${id}/enter/Logarithm`}><button className="sub-topic zoom">Logarithms</button></Link>}
+            { count === 'log' &&<Link href= {`/${id}/enter/Logarithm`}><button className="sub-topic zoom">Function</button></Link>}
 
             { loaded && <button style={{opacity:"0.4"}} onClick={() => {setCount('logeee')}}  className="topic">Function</button >}
             { loaded && <button style={{opacity:"0.4"}} onClick={() => {setCount('loge')}}  className="topic">Calculus</button >}
