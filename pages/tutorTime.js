73fc7fdedfb9296cb6053c338e962fdc60b529ae
Kamal <kamal.raghe33@gmail.com>
2025-05-17 16:54:42 -0400

    commit

--- pages/tutorTime.js
+++ pages/tutorTime.js
@@ -1,5 +1,7 @@
 import TomorrowSchedule from "@/components/tutorTime";
+import { useState } from "react";
 
+const [days,setDays] = useState([1,2,3,4,5,6])
 export default function Schedule() {
     return (
       <div className="p-6">
