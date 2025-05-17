02fc0444abcc23187c331e99780cd478a930eae8
Kamal <kamal.raghe33@gmail.com>
2025-05-17 16:50:04 -0400

    commit

--- pages/tutorTime.js
+++ pages/tutorTime.js
@@ -1,17 +1,10 @@
 import TomorrowSchedule from "@/components/tutorTime";
 
 export default function TomorrowSchedule() {
-    const hours = Array.from({ length: 8 }, (_, i) => i + 10); // [10, 11, ..., 17]
-  
-    const handleClick = (hour) => {
-      const tomorrow = new Date();
-      tomorrow.setDate(tomorrow.getDate() + 1);
-      tomorrow.setHours(hour, 0, 0, 0);
-      console.log('Scheduled Time:', tomorrow.toString());
-    };
   
     return (
       <div className="p-6">
+        <TomorrowSchedule day={1}  ></TomorrowSchedule>
       </div>
     );
   }
