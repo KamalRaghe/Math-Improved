a431af7a4931b57f16e0af54136b766761f71681
Kamal <kamal.raghe33@gmail.com>
2025-05-24 18:31:49 -0400

    commit

--- pages/discord.js
+++ pages/discord.js
@@ -1,6 +1,6 @@
 export default function Discord(){
 return(
-    <div className="center" >
+    <div className="center" style={{height:"100vh"}} >
         <input placeholder="Discord username" ></input><button>Enter</button>
     </div>
 )}
