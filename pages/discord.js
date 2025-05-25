e477e8d01b19aede1c09e19065db9a2aad285263
Kamal <kamal.raghe33@gmail.com>
2025-05-24 21:20:58 -0400

    commit

--- pages/discord.js
+++ pages/discord.js
@@ -2,22 +2,24 @@ import { useState } from "react";
 
 export default function Discord() {
   const [username, setUsername] = useState("");
+  const [enter,setEnter] = useState(false)
 
   const handleSubmit = () => {
     window.localStorage.setItem("discord", username);
+    setEnter(true)
     // You can add validation or submission logic here
   };
 
   return (
     <div className="center" style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-      <input
+      {<input
         type="text"
         placeholder="Discord username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         style={{marginBottom: "1rem", width: "250px" }}
-      />
-      <button onClick={handleSubmit}>Enter</button>
+      />}
+      {<button onClick={handleSubmit}>Enter</button>}
     </div>
   );
 }
