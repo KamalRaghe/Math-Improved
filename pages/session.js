e51f97309a4cd7f848be48f2ee5aad4e30b8b390
Kamal <kamal.raghe33@gmail.com>
2025-05-26 12:09:26 -0400

    commit

--- pages/session.js
+++ pages/session.js
@@ -30,6 +30,7 @@ export default function Confirm() {
         <div className='center column' style={{height:"100vh"}}>
             <h1 className='center'>Your session is on</h1>
             <h2>{formattedDate || "Loading date..."}</h2>
+            <br></br>
             <div className="font" >Send a friend request to <span style={{fontWeight:"bold"}} >kamal#0032</span></div>
         </div>
     );
