dbbde35cafe96b22b77c31f8a05ad2f6ffb73bf4
Kamal <kamal.raghe33@gmail.com>
2025-06-07 15:15:01 -0400

    commit

--- pages/dugsiO.js
+++ pages/dugsiO.js
@@ -36,10 +36,6 @@ export default function Home() {
             }
           });
       };
-  
-      useEffect(()=>{
-        
-      },[])
       return(
         <div className='container double center'>
             <div>
