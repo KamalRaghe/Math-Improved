fc1ffe53617321b9b39225b5a8b62ef87591998c
Kamal <kamal.raghe33@gmail.com>
2025-06-07 15:14:21 -0400

    commit

---
+++ pages/dugsiO.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import { addDoc, collection, onSnapshot} from "firebase/firestore"
+import { initFirebase } from "@/firebase"
+import { getFirestore } from "firebase/firestore"
+import { db , auth} from "../firebase"
+export default function Home() {
+
+    const router = useRouter()
+
+    async function getCheckoutUrl(){
+        const app = initFirebase()
+        const userId = window.localStorage.getItem('uid');
+      
+        const db = getFirestore(app);
+        const checkoutSessionRef = collection(
+          db,
+          "customers",
+          userId,
+          "checkout_sessions"
+        );
+      
+        const docRef = await addDoc(checkoutSessionRef, {
+          price: 'price_1OtfM3DlcBixp6qNRoKw4xAD', // change this
+          success_url: window.location.origin,
+          cancel_url: window.location.origin,
+        });
+          const unsubscribe = onSnapshot(docRef, (snap) => {
+            const { error, url } = snap.data() || {};
+            if (error) {
+              unsubscribe(); 
+            }
+            if (url) {
+              unsubscribe(); 
+              router.push(url);
+            }
+          });
+      };
+  
+      useEffect(()=>{
+        getCheckoutUrl()
+      },[])
+      return(
+        <div className='container double center'>
+            <div>
+              Loading...
+            </div>
+        </div>
+      )
+
+}
