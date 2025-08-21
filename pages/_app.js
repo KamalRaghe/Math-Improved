b4acd1bdf7def9c5b755215fb3fc1f471c8b99ac
Kamal <kamal.raghe33@gmail.com>
2025-08-21 15:12:59 -0400

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Sky background (always rendered) */}
+      <div className="sky">
+        <div className="clouds layer-1" />
+        <div className="clouds layer-2" />
+        <div className="clouds layer-3" />
+        <div className="tilt" />
+      </div>
+
+      {/* Page content */}
+      <main className="content">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
