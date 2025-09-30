601aa736f09399cd94e21a37776474a037a02a07
Kamal <kamal.raghe33@gmail.com>
2025-09-30 18:28:06 -0400

    commit

--- components/ExtraAdd.js
+++ components/ExtraAdd.js
@@ -70,7 +70,7 @@ function ExtraAdd({close, num1 , num2}){
     mix()
    },[])
         return(
-           <div className="Help" style={{zIndex:'20', border:'10px solid yellow'}}>
+           <div className="Help" >
                {extra && <HelpAdd close={Extra} num1 ={number1[count]%10} num2 = {number2[count]%10} num3={num4} />}
                <div className='cancel' style={{width:"100%"}} ><button className='cancel-btn' onClick = {close}>X</button></div>
                <span className='center'>{carry && done && <button className='carry absolute Green center' style={{fontSize:"30px",left:'145px',bottom:"450px"}} onClick={click}>Click</button>}</span>
