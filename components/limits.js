7524ad796d0a26059bc07933780f160142f23280
Kamal <kamal.raghe33@gmail.com>
2025-10-14 16:51:12 -0400

    commit

--- components/limits.js
+++ components/limits.js
@@ -61,7 +61,7 @@ export default function Limits({c,e,x,close}){
                 + {c[1]}x <span style={{fontSize:'20px',position:'relative',left:'-5px', top:"-13px"}}>{e[1]}</span>
                 + {c[2]}x <span style={{fontSize:'20px',position:'relative',left:'-5px', top:"-13px"}}>{e[2]}</span>
                </div>
-               <div className="double">
+               <div className="double" style={{margin:"20px"}}>
                =    {c[0]}x <span style={{fontSize:'20px',position:'relative',left:'-5px', top:"-13px"}}>{e[0]}</span>
                     + {c[1]}x <span style={{fontSize:'20px',position:'relative',left:'-5px', top:"-13px"}}>{e[1]}</span>
                     + {c[2]}x <span style={{fontSize:'20px',position:'relative',left:'-5px', top:"-13px"}}>{e[2]}</span>
