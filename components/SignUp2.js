6ed8a6bb67b54fe4d03b0f11b519ce0b22a19e96
Kamal <kamal.raghe33@gmail.com>
2025-11-01 15:45:00 -0400

    commit

--- components/SignUp2.js
+++ components/SignUp2.js
@@ -10,7 +10,7 @@ export default function Sign2(){
         setCheck(window.localStorage.getItem('Check'))
     })
     return(
-        <div className="center zoom" style={{backgroundColor:"beige",zIndex:"200",width:"100%",height:"100%",position:"fixed"}} >
+        <div className="center zoom" style={{zIndex:"200",width:"100%",height:"100%",position:"fixed"}} >
             <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown',backgroundColor:"beige"}}>
                 <div className="font" > You reach the limit</div>
                 <div><button className="sub-topic" onClick={()=>{router.push('/Sign')}} >Sign up</button></div>
