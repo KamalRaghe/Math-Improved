cfc8433f8aaa84cb9c5493294b9125a5c11896c1
Kamal <kamal.raghe33@gmail.com>
2025-11-01 15:46:22 -0400

    commit

--- components/SignUp2.js
+++ components/SignUp2.js
@@ -11,7 +11,7 @@ export default function Sign2(){
     })
     return(
         <div className="center zoom" style={{zIndex:"200",width:"100%",height:"100%",position:"fixed"}} >
-            <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown',backgroundColor:"beige"}}>
+            <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown'}}>
                 <div className="font" > You reach the limit</div>
                 <div><button className="sub-topic" onClick={()=>{router.push('/Sign')}} >Sign up</button></div>
                 {!(parseInt(free) === parseInt(check) - 34521) && <div className="font" > 3 day free trial available</div>}
