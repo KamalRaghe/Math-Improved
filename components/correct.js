15e853319f1d997905c3f8e214b357068b17d716
Kamal <kamal.raghe33@gmail.com>
2025-11-01 15:41:58 -0400

    commit

--- components/correct.js
+++ components/correct.js
@@ -1,6 +1,6 @@
 function Correct(){
     return(
-        <div className='center beige absolute' style={{zIndex:"100",width:"100vw"}} >
+        <div className='center absolute' style={{zIndex:"100",width:"100vw"}} >
                 <img className="check" style={{borderRadius:"50%",width:'500px',height:"500px",position:"absolute",backgroundColor:"#a0e1ff"}} src={'/Green.png'} alt="Correct" />
             </div> 
     )
