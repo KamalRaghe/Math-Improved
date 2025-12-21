6d46a469b99f4149879626c20ee70636441b4665
Kamal <kamal.raghe33@gmail.com>
2025-12-20 21:15:25 -0500

    commit

--- components/SignUp2.js
+++ components/SignUp2.js
@@ -10,8 +10,8 @@ export default function Sign2(){
         setCheck(window.localStorage.getItem('Check'))
     })
     return(
-        <div className="center zoom" style={{background:"beige",zIndex:"200",width:"100%",height:"100%",position:"fixed"}} >
-            <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown'}}>
+        <div className="center zoom" style={{zIndex:"200",width:"100%",height:"100%",position:"fixed"}} >
+            <div className=" column center" style={{background:"beige",borderRadius:"20px",padding:"20px",border:'2px solid brown'}}>
                 <div className="font" > You reach the limit</div>
                 <div><button className="sub-topic" onClick={()=>{router.push('/Sign')}} >Sign up</button></div>
                 {!(parseInt(free) === parseInt(check) - 34521) && <div className="font" > 3 day free trial available</div>}
