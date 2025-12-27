870d18b30c669461556602ecbe9b79215a02a0f7
Kamal <kamal.raghe33@gmail.com>
2025-12-27 14:51:14 -0500

    commit

--- components/limits.js
+++ components/limits.js
@@ -90,7 +90,7 @@ export default function Limits({c,e,x,close}){
                </div>}
                {steps > 4 && <div style={{marginTop:"20px",width:"100%",fontSize:"25px",color:"black"}}>
                =    {steps > 4 && <span style={{margin:"5px"}} > {c[0]*(x**e[0])}</span>}
-                    + {steps > 4 && <span style={{margin:"5px"}} > {c[0]*(x**e[0])}</span>}
+                    + {steps > 4 && <span style={{margin:"5px"}} > {c[1]*(x**e[1])}</span>}
                     + {steps > 4 && <span style={{margin:"5px"}} > {c[0]*(x**e[0])}</span>}
                </div>}
                <div className="box center" style={{width:"100%",height:"60%",alignItems:"end"}} >
