cf011d9a23d249972376af7bd89cdb3250142b5d
Kamal <kamal.raghe33@gmail.com>
2026-01-04 15:26:36 -0500

    commit

--- components/feedback.js
+++ components/feedback.js
@@ -22,7 +22,7 @@ export default function FeedBack({close}){
     
     return(
         <div className="center zoom" style={{zIndex:"200",width:"100%",height:"100%",position:"fixed",right:"0px"}} >
-            <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown'}}>
+            <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown',backgroundColor:"beige"}}>
                  <div className='cancel' style={{width:"100%"}} >
                  <button className='cancel-btn' style={{fontSize:"25px",margin:"0px",position:"relative",left:"6px",bottom:"10px",alignItems:"end",zIndex:"100"}}  onClick={()=>close()}>X</button>
                 </div>
