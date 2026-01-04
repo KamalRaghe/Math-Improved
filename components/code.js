87dc7b9a38419eb13fe9b7fec74cef013f67e49e
Kamal <kamal.raghe33@gmail.com>
2026-01-04 15:43:15 -0500

    commit

--- components/code.js
+++ components/code.js
@@ -12,6 +12,7 @@ export default function Code({close}){
         const email = window.localStorage.getItem('User')
         if(post){
           window.localStorage.setItem('code',post)
+          close()
         }else{
             close()
         }    
@@ -23,7 +24,7 @@ export default function Code({close}){
                  <div className='cancel' style={{width:"100%"}} >
                  <button className='cancel-btn' style={{fontSize:"25px",margin:"0px",position:"relative",left:"6px",bottom:"10px",alignItems:"end",zIndex:"100"}}  onClick={()=>close()}>X</button>
                 </div>
-                 <textarea placeholder="Feedback" onChange={(e)=>{setPost(e.target.value)}} ></textarea>
+                 <textarea placeholder="Code" onChange={(e)=>{setPost(e.target.value)}} ></textarea>
                  <br></br>
                  <button onClick={NewFeedback}>Submit</button>
             </div>
