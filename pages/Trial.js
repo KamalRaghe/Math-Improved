9102c70ad927e06d757395e1f103b8f667a3e0d8
Kamal <kamal.raghe33@gmail.com>
2026-01-04 15:16:27 -0500

    commit

--- pages/Trial.js
+++ pages/Trial.js
@@ -43,8 +43,7 @@ export default function Math(){
                     <div ><button className="sub-topic" onClick={()=>{router.push('/Sign')}}>Sign in</button></div>
                     <div><button className="sub-topic" style={{position:"relative",bottom:"20px"}} onClick={()=>{setCheck(true);setNone('none'),setFlex('none')}} >Feedback</button></div>
                     <div ><button className="sub-topic" style={{position:"relative",bottom:"40px"}} onClick={()=>{setAbout(true);setNone('none'),setFlex('none')}} >About us</button></div>
-                    <div ><button className="sub-topic" style={{position:"relative",bottom:"40px"}} onClick={()=>{setAbout(true);setNone('none'),setFlex('none')}} >Code</button></div>
-                </div>
+                    
             </div>
             <div className="column" style={{display:none,paddingBottom:"20px"}} onClick={close} >
                 {loaded && <button onClick={() => {setCount(1)}} className="topic">Addition</button >}
