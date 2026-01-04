6a14132367dec83c0db5b0ab6f7d8d825a9d9404
Kamal <kamal.raghe33@gmail.com>
2026-01-04 15:18:39 -0500

    commit

--- pages/Trial.js
+++ pages/Trial.js
@@ -32,6 +32,7 @@ export default function Math(){
 
     return (
         <div className="beige" style={{padding:"20px",paddingBottom:"50px"}}>
+
             {check && <FeedBack close={()=>{setCheck(false),setNone(''),setFlex('flex')}} />}
             {about &&<AboutUs close={()=>{setAbout(false),setNone(''),setFlex('flex')}} />}
             <div style={{display:flex,justifyContent:"space-between",width:"100vw",height:"180px"}}>
@@ -41,9 +42,9 @@ export default function Math(){
                 </div>
                 <div className=" center column relative" style={{marginLeft:"50px",width:"300px",justifyContent:"end",top:"50px"}}>
                     <div ><button className="sub-topic" onClick={()=>{router.push('/Sign')}}>Sign in</button></div>
+                    <div ><button className="sub-topic" onClick={()=>{setAbout(true);setNone('none'),setFlex('none')}} >About us</button></div>
                     <div><button className="sub-topic" style={{position:"relative",bottom:"20px"}} onClick={()=>{setCheck(true);setNone('none'),setFlex('none')}} >Feedback</button></div>
                     <div ><button className="sub-topic" style={{position:"relative",bottom:"40px"}} onClick={()=>{setAbout(true);setNone('none'),setFlex('none')}} >About us</button></div>
-
                 </div>
             </div>
             <div className="column" style={{display:none,paddingBottom:"20px"}} onClick={close} >
