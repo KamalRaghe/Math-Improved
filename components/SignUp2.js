a53af125bf4f7e50c092f86f95475a2dd7f9c9cf
Kamal <kamal.raghe33@gmail.com>
2026-01-04 15:35:53 -0500

    commit

--- components/SignUp2.js
+++ components/SignUp2.js
@@ -5,6 +5,7 @@ export default function Sign2(){
     const router = useRouter()
     const [free, setFree] = useState(false)
     const [check, setCheck] = useState(false)
+    const [check, setCheck] = useState(false)
     useEffect(()=>{
         setFree(window.localStorage.getItem('userId'))
         setCheck(window.localStorage.getItem('Check'))
