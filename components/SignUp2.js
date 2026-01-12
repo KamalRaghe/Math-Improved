4517ae35abb35ffda45185e77ecf03a936ee521b
Kamal <kamal.raghe33@gmail.com>
2026-01-11 23:44:59 -0500

    commit

--- components/SignUp2.js
+++ components/SignUp2.js
@@ -11,17 +11,17 @@ export default function Sign2(){
         setCheck(window.localStorage.getItem('Check'))
         setCode(window.localStorage.getItem('code'))
     })
-    return(
-      <div className="center" >
-          {code != 'nile123' && <div className="center zoom" style={{zIndex:"200",width:"100%",height:"100%",position:"fixed"}} >
-            <div className=" column center" style={{background:"beige",borderRadius:"20px",padding:"20px",border:'2px solid brown'}}>
-                <div className="font" > You reach the limit</div>
-                <div><button className="sub-topic" onClick={()=>{router.push('/Sign')}} >Sign up</button></div>
-                {!(parseInt(free) === parseInt(check) - 34521) && <div className="font" > 3 day free trial available</div>}
-            </div>
-        </div>}
-      </div>
-    )
-    
-   
+    if(code != 'nile123'){
+        return(
+        <div className="center" >
+          <div className="center zoom" style={{zIndex:"200",width:"100%",height:"100%",position:"fixed"}} >
+              <div className=" column center" style={{background:"beige",borderRadius:"20px",padding:"20px",border:'2px solid brown'}}>
+                  <div className="font" > You reach the limit</div>
+                  <div><button className="sub-topic" onClick={()=>{router.push('/Sign')}} >Sign up</button></div>
+                  {!(parseInt(free) === parseInt(check) - 34521) && <div className="font" > 3 day free trial available</div>}
+              </div>
+          </div>
+        </div>
+      )
+    }
 }
