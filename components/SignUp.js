12d66f9e0bfc1da71c36a555b1620727ee56422d
Kamal <kamal.raghe33@gmail.com>
2026-01-11 23:43:24 -0500

    commit

--- components/SignUp.js
+++ components/SignUp.js
@@ -11,19 +11,19 @@ export default function Sign(){
         setCheck(window.localStorage.getItem('Check'))
         setCode(window.localStorage.getItem('code'))
     })
-    return(
-        <div className="center zoom" style={{zIndex:"200",width:"100%",height:"100%",position:"fixed"}} >
-           {code != 'nile123' && <div className=" column center" style={{background:"beige",borderRadius:"20px",padding:"20px",border:'2px solid brown'}}>
-                <div className="font" > You reach the limit for this topic</div>
-                <br></br>
-                <div className="font">Try a new topic </div>
-                <br></br>
-                <div>or</div>
-                <div><button className="sub-topic" onClick={()=>{router.push('/Sign')}} >Sign up</button></div>
-                {!(parseInt(free) === parseInt(check) - 34521) && <div className="font" > 3 day free trial available</div>}
-            </div>}
-        </div>
-    )
-    
-   
+    if(code != 'nile123'){
+        return(
+            <div className="center zoom" style={{zIndex:"200",width:"100%",height:"100%",position:"fixed"}} >
+            <div className=" column center" style={{background:"beige",borderRadius:"20px",padding:"20px",border:'2px solid brown'}}>
+                    <div className="font" > You reach the limit for this topic</div>
+                    <br></br>
+                    <div className="font">Try a new topic </div>
+                    <br></br>
+                    <div>or</div>
+                    <div><button className="sub-topic" onClick={()=>{router.push('/Sign')}} >Sign up</button></div>
+                    {!(parseInt(free) === parseInt(check) - 34521) && <div className="font" > 3 day free trial available</div>}
+                </div>
+            </div>
+        )
+        }
 }
