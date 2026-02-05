4903b314dc31f8bfe356813b32182f741c9929ca
Kamal <kamal.raghe33@gmail.com>
2026-02-04 22:28:14 -0500

    commit

--- pages/Trial.js
+++ pages/Trial.js
@@ -59,7 +59,7 @@ export default function Math(){
                     { count === 1 && <div style={{margin:"0",padding:"0",display:"flex"}} >
                         <Link href= {`/MIT/singleAdd`}><button className="sub-topic zoom" style={{margin:"0 20px 0 40px"}}>Single digit Addition</button></Link>
                         <Link href= {`/MIT/MAdd`}><button className="sub-topic zoom green"  style={{margin:"0"}} >Memersois</button></Link>
-                    </div>}
+                    </div>}lsDfo
                     { count === 1 && <Link href= {`/MIT/doubleAdd`}><button className="sub-topic zoom" style ={{bottom:"60px"}} >Double digit Addition</button></Link>}
                 
                 { loaded && <button onClick={() => {setCount(2)}}  className="topic">Subtraction</button >}
