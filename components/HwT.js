e33c177b5b7094d31cf712da5a9deda3b98dae9b
Kamal <kamal.raghe33@gmail.com>
2026-03-04 17:04:59 -0500

    commit

--- components/HwT.js
+++ components/HwT.js
@@ -44,7 +44,9 @@ export default function TWH({close}){
     console.error("Error adding feedback:", error);
   }
 }
-    
+    useEffect(()=>
+    console.log(help)
+    ,[help])
     return(
         <div className="center zoom" style={{zIndex:"200",width:"100%",height:"100%",position:"fixed",right:"0px"}} >
             <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown',backgroundColor:"beige"}}>
