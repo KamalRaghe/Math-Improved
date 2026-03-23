76f9ec2897acc4f7762d5925bb96978cbaf7b42e
Kamal <kamal.raghe33@gmail.com>
2026-03-23 13:48:02 -0400

    commit

--- pages/viewClass.js
+++ pages/viewClass.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/router"
 
 export default function View(){
     const [data, setData] = useState()
-
+    const [name, setName] = useState()
     async function Name(n){
         const querySnapshot = await getDocs(collection(db, n));
         const list = querySnapshot.docs.map((doc) => ({
@@ -20,19 +20,29 @@ export default function View(){
     useEffect(()=>{
         const n = window.localStorage.getItem("Name")
         Name(n)
+        setName(n)
     },[])
     return(
         <div>
+            <div className="center" >
+                <span style={{position:"relative",left:"110px"}} className="double">{name}</span>
+                <button style={{marginLeft:"212px"}} className="choice green" >
+                    state
+                </button> 
+                <button className="choice red" >
+                    mistake
+                </button>
+            </div>
             {data && <div>
                 {data.map((item) => (
                 <div className=" center" key={item.id}>
                     {item.name &&<span className='double' style={{width:"300px"}} >
                         {item.name} {item.last}
                     </span>} 
-                    {<button className="choice green" >
+                    {item.state && <button className="choice green" >
                         {item.state}
                     </button>} 
-                    {<button className="choice red" >
+                    {item.state && <button className="choice red" >
                         {item.mistake}
                     </button>}
                 </div>
