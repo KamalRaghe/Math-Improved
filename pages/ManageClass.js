bdac476c8aa6b4a6522896f4b70274b6e0845c86
Kamal <kamal.raghe33@gmail.com>
2026-03-23 12:51:18 -0400

    commit

--- pages/ManageClass.js
+++ pages/ManageClass.js
@@ -1,13 +1,34 @@
 import { useState } from "react"
+import { getDocs, collection } from "firebase/firestore"
+import { db } from "@/firebase"
 
 
 export default function Manage(){
     const [name, setName] = useState()
+    const [n, setN] = useState()
+    const [data, setData] = useState()
+
+    async function saveName(){
+        window.localStorage.setItem('Name',n)
+        setName(n)
+        const querySnapshot = await getDocs(collection(db, n));
+        setData(querySnapshot)
+        console.log('cwi')
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, doc.data());
+        });
+    }
+
     return(
-        <div className="center">
-            <h1>Enter Name</h1>
-            <br></br>
-            <input placeholder="Name" onChange={(e)=>{setName(e.target.value)}} ></input>
-        </div>
+        <>
+            {!name ? <div className="center column">
+                <h1>Enter Name</h1>
+                <div>
+                    <input placeholder="Name" onChange={(e)=>{setN(e.target.value)}} ></input> <button onClick={saveName} >Enter</button>
+                </div>
+            </div>:<div>
+                <button className="choice helo" >Assign Homework</button>
+            </div>}
+        </>
     )
 }
