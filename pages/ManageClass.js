d2e6023bdc3206fe8accd7f77aedf08d04990967
Kamal <kamal.raghe33@gmail.com>
2026-03-23 13:08:27 -0400

    commit

--- pages/ManageClass.js
+++ pages/ManageClass.js
@@ -13,12 +13,7 @@ export default function Manage(){
     async function saveName(){
         window.localStorage.setItem('Name',n)
         setName(n)
-        const querySnapshot = await getDocs(collection(db, n));
-        setData(querySnapshot)
-        console.log('cwi')
-        querySnapshot.forEach((doc) => {
-            console.log(doc.id, doc.data());
-        });
+       
     }
 
     useEffect(()=>{
