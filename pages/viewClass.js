27869c05275893470efca79751cbc85239a02793
Kamal <kamal.raghe33@gmail.com>
2026-03-23 13:09:15 -0400

    commit

--- pages/viewClass.js
+++ pages/viewClass.js
@@ -1,8 +1,10 @@
-
+import { useEffect, useState } from "react"
+import { getDocs, collection } from "firebase/firestore"
+import { db } from "@/firebase"
+import { useRouter } from "next/router"
 
 export default function View(){
      async function saveName(){
-        )
         const querySnapshot = await getDocs(collection(db, n));
         setData(querySnapshot)
         console.log('cwi')
