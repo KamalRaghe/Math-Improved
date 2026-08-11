4896d3daaf80e71ef5d6de75867ffc147d065b87
Kamal Raghe <Kamal.raghe33@gmail.com>
2026-08-11 16:33:54 -0400

    kamal

--- pages/Games/Doubledigitaddition.js
+++ pages/Games/Doubledigitaddition.js
@@ -56,23 +56,24 @@ export default function DoubleAdd(){
         setCorrect(true)
         setTimeout(() => {
             setCorrect(false) 
-        }, 1200);
+        }, 500);
       }
   
       function WrongA(){ 
         setWrong(true)
         setTimeout(() => {
             setWrong(false) 
-        }, 1200);
+        }, 500);
       } 
     function Add(){
+        if(!wrong && !correct){
         setTimeout(() => {
             setNum1(Math.floor(Math.random()*90+10))
             setNum2(Math.floor(Math.random()*90+10))
             mix()
             setNum3(prevChange => prevChange.sort((a,b)=>Math.random()-0.5)) 
-        }, 1200)
-    }
+        }, 500)
+    }}
 
     useEffect(() =>{
         mix()
