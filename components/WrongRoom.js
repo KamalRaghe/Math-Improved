d07835ecc6f7138d6e589022718310f6e2cd3b5c
Kamal Raghe <Kamal.raghe33@gmail.com>
2026-09-14 14:15:00 -0400

    Update WrongRoom

--- components/WrongRoom.js
+++ components/WrongRoom.js
@@ -11,7 +11,7 @@ export default function WrongRoom({close,remove}){
     })
     return(
         <div className="center zoom" style={{zIndex:"200",width:"100%",height:"100%",position:"absolute"}} >
-            <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown',backgroundColor:"white",width:"300px",height:"150px"}} >
+            <div className=" column center" style={{borderRadius:"20px",padding:"20px",border:'2px solid brown',backgroundColor:"beige",width:"300px",height:"150px"}} >
                 <div style={{marginBottom:"5px"}} >Do you want to leave the room?</div>
                 <div className="center" >
                     <button className="green choice" onClick={remove} >Yes</button>
